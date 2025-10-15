48a20939d200e2edffb8e80cd26e32fbf3b9f805
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-10-15 23:03:39 +0100

    Created a script to autodeploy docker database and initialise it

---
+++ services/app.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import subprocess
+import requests
+import time
+
+def restart_docker_postgres():
+    base = Path(__file__).parent.parent
+    scripts_dir = base / "devops/docker/postgres"
+
+    # Destroy existing instance + connected volumes
+    subprocess.run(["docker", "compose", "down", "-v"], cwd=scripts_dir)
+    # Restart Docker instance
+    subprocess.run(["docker", "compose", "up", "-d"], cwd=scripts_dir)
+
+def start_api_server():
+    base = Path(__file__).parent
+    scripts_dir = base / "api"
+
+    subprocess.Popen(
+        ["python3", "api.py"], 
+        cwd=scripts_dir
+    )
+
+    time.sleep(5)
+
+    base_url = "http://REDACTED_IP:5000/"
+    response = requests.get(base_url+"init_database")
+    print(response.status_code, response.text)
+
+if __name__ == "__main__":
+    try:
+        restart_docker_postgres()
+        start_api_server()
+    except:
+        pass
+    
