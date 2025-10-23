3b71ebef056e05972566271b3c5f8c36826a9393
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-10-23 21:22:50 +0100

    Added comment for other buckets to consider

--- services/scripts/init_minio.py
+++ services/scripts/init_minio.py
@@ -7,6 +7,8 @@ client = Minio(
     secure=False
 )
 
+# Consider creating buckets for audio, video and text so that it splits types of content?
+
 # Makes buckets (not if they already exist) for each type of category of files I want to store
 for bucket in ["bible-dbl-raw", "open-bible-location-data", "bible-nlp"]:
     if not client.bucket_exists(bucket):
