5517bbeba75ce4f56b99c46e9823e35884feb8c0
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-10-31 00:39:44 +0000

    chore: cleaned up logging line

--- services/ingestor/book.py
+++ services/ingestor/book.py
@@ -63,5 +63,5 @@ class Book:
             additions += 1
         
         if additions > 0:
-            print(f"[{additions}] Chapters added for {self.book_code}")
+            print(f"[{additions}] Chapters added for {self.book_code[0]}")
    
