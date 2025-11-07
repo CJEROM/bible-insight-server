8523bcb88a83a7f0157063b1fb45fa6dfc6a37ad
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-11-07 17:49:59 +0000

    moved standardizing dashes function as well as started on get_note_type function to figure out if this is a cross reference or a footnote

--- services/ingestor/translationnote.py
+++ services/ingestor/translationnote.py
@@ -9,7 +9,16 @@ class TranslationNote:
         """,
     }
 
-    def __init__(self, book_map_id:int, translation_id:int, note_xml:Tag, db_conn, parent:int=None, note_type:str=None):
+    def standardise_dash(self, ref):
+        new_ref = ref
+        dash_formats = ["–", "—", "−", "–"] # Different dashes used
+        for dash in dash_formats:
+            if dash in ref:
+                new_ref = ref.replace(dash, "-")
+
+        return new_ref
+
+    def __init__(self, book_map_id:int, translation_id:int, note_xml:Tag, db_conn, parent:int=None, param_note_type:str=None):
         self.book_map_id = book_map_id
         self.translation_id = translation_id
         self.note_xml = note_xml
@@ -17,6 +26,9 @@ class TranslationNote:
         self.conn = db_conn
         self.cur = self.conn.cursor()
 
+        self.note_type = self.get_note_type(param_note_type)
+        if self.note_type == None:
+            return # if note not valid
 
         
         self.source_ref = None
@@ -24,7 +36,17 @@ class TranslationNote:
 
         self.conn.commit()
 
-    def get_note_type(self):
+    def get_note_type(self, param_note_type):
+        # if passed into class properly then return that
+        if param_note_type != None:
+            return param_note_type
+        
+        # if not then figure out from note_xml
+        note_style = self.note_xml.get("style")
+        if note_style == "f": # footnote
+            pass
+        if note_style == "x": # cross reference
+            pass
         pass
 
     def get_source_ref(self):
