8eea08bb0882fb4b508fba57e4782f52eca70e3e
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-11-13 12:26:01 +0000

    typecast word as string so properly writes to label studio

--- services/labeller/labeller.py
+++ services/labeller/labeller.py
@@ -211,7 +211,7 @@ class Labeller:
                 word_id = self.cur.fetchone()[0]
                 words_added.append(word_id)
 
-                task = {"data": {"text": word, "word_id": word_id}}
+                task = {"data": {"text": str(word), "word_id": word_id}}
 
                 # Add prelabelled prediction if it's NLP-tagged
                 if is_nlp:
