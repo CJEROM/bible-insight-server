bc45c8de15e1d2011e6f4946d3b773fb15dc86c1
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-11-15 13:49:03 +0000

    adjusted all queries correcly + added switch statement for adding the data for those queries depending on type of node we ran into

--- services/ingestor/nodes.py
+++ services/ingestor/nodes.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag, NavigableString
 import psycopg2
 
 from pathlib import Path
@@ -37,41 +37,42 @@ class Nodes:
             RETURNING id;
         """,
         "chapter": """
-            INSERT INTO bible.nodes (node_type, style, encoding) 
-            VALUES ('chapter', %s, %s)
+            INSERT INTO bible.nodes (node_type, style, number, sid, eid) 
+            VALUES ('chapter', %s, %s, %s, %s)
             RETURNING id;
         """,
         "para": """
-            INSERT INTO bible.nodes (node_type, style, encoding) 
+            INSERT INTO bible.nodes (node_type, style, vid) 
             VALUES ('para', %s, %s)
             RETURNING id;
         """,
         "verse": """
-            INSERT INTO bible.nodes (node_type, style, encoding) 
-            VALUES ('verse', %s, %s)
+            INSERT INTO bible.nodes (node_type, style, number, sid, eid) 
+            VALUES ('verse', %s, %s, %s, %s)
             RETURNING id;
         """,
         "note": """
-            INSERT INTO bible.nodes (node_type, style, encoding) 
+            INSERT INTO bible.nodes (node_type, style, caller) 
             VALUES ('note', %s, %s)
             RETURNING id;
         """,
         "char": """
-            INSERT INTO bible.nodes (node_type, style, encoding) 
-            VALUES ('char', %s, %s)
+            INSERT INTO bible.nodes (node_type, style, closed, strong) 
+            VALUES ('char', %s, %s, %s)
             RETURNING id;
         """,
         "ref": """
-            INSERT INTO bible.nodes (node_type, style, encoding) 
-            VALUES ('ref', %s, %s)
+            INSERT INTO bible.nodes (node_type, loc) 
+            VALUES ('ref', %s)
             RETURNING id;
         """,
         "text": """
-            INSERT INTO bible.nodes (node_type, encoding) 
-            VALUES ('text', %s, %s)
+            INSERT INTO bible.nodes (node_type, node_text) 
+            VALUES ('text', %s)
             RETURNING id;
         """,
     }
+
     def __init__(self, book_map_id, db_conn, book_xml):
         # Adds a database connection
         self.conn = db_conn
@@ -82,10 +83,68 @@ class Nodes:
         self.book_map_id = book_map_id
 
         for node in self.book_soup.descendants:
-            self.cur.execute(self.SQL["xml"], (None,None))
+            node_id = None # Initialise node_id for the note we are going to create in DB
+
+            if isinstance(node, Tag):
+                query = self.SQL.get(node.name)
+                match node.name:
+                    case "xml":
+                        version = node.get("version")
+                        encoding = node.get("encoding")
+
+                        node_id = self.execute_and_get_id(query, (version, encoding))
+                    case "usx":
+                        version = node.get("version")
+
+                        node_id = self.execute_and_get_id(query, (version,))
+                    case "book":
+                        style = node.get("style")
+                        code = node.get("code")
+
+                        node_id = self.execute_and_get_id(query, (style, code))
+                    case "chapter":
+                        style = node.get("style")
+                        number = node.get("number")
+                        sid = node.get("sid")
+                        eid = node.get("eid")
+
+                        node_id = self.execute_and_get_id(query, (style, number, sid, eid))
+                    case "para":
+                        style = node.get("style")
+                        vid = node.get("vid")
+                        node_id = self.execute_and_get_id(query, (style, vid))
+                    case "verse":
+                        style = node.get("style")
+                        number = node.get("number")
+                        sid = node.get("sid")
+                        eid = node.get("eid")
+
+                        node_id = self.execute_and_get_id(query, (style, number, sid, eid))
+                    case "note":
+                        style = node.get("style")
+                        caller = node.get("caller")
+
+                        node_id = self.execute_and_get_id(query, (style, caller))
+                    case "char":
+                        style = node.get("style")
+                        closed = node.get("closed")
+                        strong = node.get("strong")
+
+                        node_id = self.execute_and_get_id(query, (style, closed, strong))
+                    case "ref":
+                        loc = node.get("loc")
+
+                        node_id = self.execute_and_get_id(query, (loc,))
+
+            if isinstance(node, NavigableString):  
+                node_id = self.execute_and_get_id(self.SQL.get("text"), (None,None))
 
         pass
-
+    
+    def execute_and_get_id(self, query, params):
+        self.cur.execute(query, params)
+        return self.cur.fetchone()[0]
+    
     def walk_parsed_xml(self):
         pass
 
