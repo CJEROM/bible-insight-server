1a295abf10a7851bb0787803dc8de3f9cb169a20
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-11-18 11:48:26 +0000

    added database connection and reading environment variables

--- services/tokeniser/tokenisation.py
+++ services/tokeniser/tokenisation.py
@@ -1,4 +1,21 @@
 import spacy
+import psycopg2
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Automatically find the project root (folder containing .env)
+current = Path(__file__).resolve()
+for parent in current.parents:
+    if (parent / ".env").exists():
+        load_dotenv(parent / ".env")
+        break
+
+POSTGRES_USERNAME = os.getenv("POSTGRES_USERNAME")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
 # Decided I need to better tokenise so first will load all verses and then update the data for them later, I want tokens afterall in my database.
 
@@ -10,11 +27,21 @@ NLP_MAPPING = {
     "heb": "he"                 # HEBREW
 }
 
+# Will create tokens for one translation at a time, to preprocess it all, then carry on with the rest before moving onto others.
+
 class Tokenisation:
-    def __init__(self, verse_occurence, translation_id, language):
-        self.verse_occurence = verse_occurence
+    def __init__(self, translation_id, language):
         self.translation_id = translation_id
         self.language_id, self.language_iso = language
+
+        # Adds a database connection
+        self.conn = psycopg2.connect(
+            host=POSTGRES_HOST,
+            port=POSTGRES_PORT,
+            dbname=POSTGRES_DB,
+            user=POSTGRES_USERNAME,
+            password=POSTGRES_PASSWORD
+        )
 
         self.nlp = None
 
