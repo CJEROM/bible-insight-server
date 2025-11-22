f07d28b25db92fc9f48e157afda4865a299ad486
CJEROM <118741301+CJEROM@users.noreply.github.com>
2025-11-22 18:23:21 +0000

    bugfix: changed absolute file path to relative one to find downloads folder for new translations (whether on mac or windows) + adjusted tests

--- services/ingestor/ingestor.py
+++ services/ingestor/ingestor.py
@@ -41,7 +41,7 @@ class Ingestor:
         self.start_time = time.time()
 
         # Folder where you want downloads to go
-        self.download_path = "C:/Users/CephJ/Documents/git/bible-insight-server/downloads"
+        self.download_path = Path(__file__).parents[2] / "downloads"
         os.makedirs(self.download_path, exist_ok=True)
 
         # Passes Minio client connection on to the MinioUSXUpload class
@@ -136,7 +136,7 @@ class Ingestor:
     def get_downloads(self):
         with sync_playwright() as p:
             # Launch browser
-            browser = p.chromium.launch(headless=False)  # headless=False shows the browser
+            browser = p.chromium.launch(headless=False, timeout=999999)  # headless=False shows the browser
             context = browser.new_context(accept_downloads=True)  # Important to handle downloads
 
             page = context.new_page()
@@ -167,11 +167,11 @@ class Ingestor:
             """)
 
             translations = self.cur.fetchall()
-            # translations = (
-            #     "04da588535d2f823-240018".split("-"),
-            #     "32339cf2f720ff8e-265856".split("-"),
-            #     "7644de2e4c5188e5-265855".split("-")
-            # )
+            translations = (
+                "32664dc3288a28df-265137".split("-"),
+                "6bab4d6c61b31b80-252265".split("-")
+                
+            )
 
             for dbl_id, agreement_id in translations:
 
