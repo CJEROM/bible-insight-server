f7760cf6f3d76047f7cb5857656e47665b67e5cc
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-12-06 12:47:53 +0000

    chore: added validation of parent manager, if not then create an uncoupled manager instance

--- services/utilities/dbmanager.py
+++ services/utilities/dbmanager.py
@@ -2,9 +2,16 @@ from envmanager import EnvManager
 import psycopg2
 from psycopg2.extras import execute_values
 
+from managerhandler import ManagerHandler
+
 class DBManager:
-    def __init__(self):
-        self.env = EnvManager()
+    def __init__(self, this_manager: ManagerHandler = None):
+        self.this_manager = this_manager
+        self.env = None
+        if this_manager == None:
+            self.env = EnvManager()
+        else:
+            self.env = self.this_manager.get_env()
 
         config = self.env.get_postgres_config()
 
