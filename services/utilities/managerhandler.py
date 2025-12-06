0bfeb3d7c7389feeeb0f6cfc16fab830332f26b2
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-12-06 12:44:31 +0000

    chore: instantiation of manager objects + getter functions

--- services/utilities/managerhandler.py
+++ services/utilities/managerhandler.py
@@ -1,9 +1,24 @@
+from dbmanager import DBManager
+from envmanager import EnvManager
 from logmanager import LogManager
+from objectmanager import ObjectManager
 
 class ManagerHandler:
     def __init__(self):
         self.logs = {}
-        pass
+        
+        self.env = EnvManager()
+        self.db = DBManager()
+        self.obj = ObjectManager()
+
+    def get_env(self):
+        return self.env
+    
+    def get_db(self):
+        return self.db
+    
+    def get_obj(self):
+        return self.obj
 
     def create_log(self, file_name):
         self.logs[file_name] = LogManager(log_file_name=file_name)
