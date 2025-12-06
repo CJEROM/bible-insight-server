ec90b3f4666c5b7cc49acddac8c17381411ae168
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-12-06 12:51:16 +0000

    chore: passing main manager object instance onto child managers

--- services/utilities/managerhandler.py
+++ services/utilities/managerhandler.py
@@ -7,9 +7,9 @@ class ManagerHandler:
     def __init__(self):
         self.logs = {}
         
-        self.env = EnvManager()
-        self.db = DBManager()
-        self.obj = ObjectManager()
+        self.env = EnvManager(self)
+        self.db = DBManager(self)
+        self.obj = ObjectManager(self)
 
     def get_env(self):
         return self.env
@@ -21,7 +21,7 @@ class ManagerHandler:
         return self.obj
 
     def create_log(self, file_name):
-        self.logs[file_name] = LogManager(log_file_name=file_name)
+        self.logs[file_name] = LogManager(manager=self, log_file_name=file_name)
 
     def return_log(self, file_name):
         requested_log = self.logs.get(file_name)
