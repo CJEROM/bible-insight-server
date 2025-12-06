61e2713349761d7451ff40366e42da605c51a706
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-12-06 11:20:51 +0000

    feature: created class for Environment Variables, init with existing logic

---
+++ services/utilities/envmanager.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Automatically find the project root (folder containing .env)
+current = Path(__file__).resolve()
+for parent in current.parents:
+    if (parent / ".env").exists():
+        load_dotenv(parent / ".env")
+        break
+
+POSTGRES_USERNAME = os.getenv("POSTGRES_USERNAME")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
+MINIO_USERNAME = os.getenv("MINIO_USERNAME")
+MINIO_PASSWORD = os.getenv("MINIO_PASSWORD")
+
+DBL_USERNAME = os.getenv("DBL_USERNAME")
+DBL_PASSWORD = os.getenv("DBL_PASSWORD")
+
+class EnvManager:
+    def __init__(self):
+        pass
+
