be7ce7aabb7f952621340fd08acca17bed8da5aa
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-12-06 11:21:13 +0000

    chore: added function for loading environment variables into memory

--- services/utilities/envmanager.py
+++ services/utilities/envmanager.py
@@ -2,13 +2,6 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-# Automatically find the project root (folder containing .env)
-current = Path(__file__).resolve()
-for parent in current.parents:
-    if (parent / ".env").exists():
-        load_dotenv(parent / ".env")
-        break
-
 POSTGRES_USERNAME = os.getenv("POSTGRES_USERNAME")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
@@ -25,4 +18,13 @@ DBL_PASSWORD = os.getenv("DBL_PASSWORD")
 class EnvManager:
     def __init__(self):
         pass
+
+    def load_env(self):
+        # Find the project root containing .env
+        current = Path(__file__).resolve()
+        for parent in current.parents:
+            env_file = parent / ".env"
+            if env_file.exists():
+                load_dotenv(env_file)
+                break
 
