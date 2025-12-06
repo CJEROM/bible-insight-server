22c9fc0d618a009839b038aca92b2b7f3c411103
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-12-06 20:42:22 +0000

    refactor: pass LogManager into Book Object instead of db_conn

--- services/ingestor/book.py
+++ services/ingestor/book.py
@@ -29,7 +29,7 @@ class Book:
 
         self.log.log_to_file(f"Created with [book_map_id:{self.book_map_id}]", f"BOOK: {self.book_code}", "INFO")
 
-        self.book_nodes =       Nodes(self.this_translation, self, db_conn, book_string) # Allows for creating all associated nodes for this book first, before going down the rest of this pipeline
+        self.book_nodes =       Nodes(self.this_translation, self, self.log, book_string) # Allows for creating all associated nodes for this book first, before going down the rest of this pipeline
         
         self.createTextChapters()
 
