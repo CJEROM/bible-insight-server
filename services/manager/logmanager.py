ffc799d67860b0d0fc98f799dec61bd56026623e
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-12-09 20:25:59 +0000

    feature: if log file name not set, by default will use now timestamp to create instead of raise error

--- services/manager/logmanager.py
+++ services/manager/logmanager.py
@@ -45,7 +45,9 @@ class LogManager():
         self.log_file_name = log_file_name
 
         if log_file_name == None:
-            raise ValueError("Missing required parameter: log_file_name.")
+            dt = datetime.now()
+            # dt.strftime("%Y-%m-%d %H:%M:%S") => 2025-12-09 20:15:42
+            log_file_name = dt.strftime("%Y%m%d%H%M%S")
         
         self.log_file = self.log_path / f"{self.log_file_name}.{self.log_file_extension}"
         self.disable_log = disable_log
