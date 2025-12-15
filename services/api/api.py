43618dabdf9aa8facf7d54dc0cb59e243fe64026
CJEROM <118741301+CJEROM@users.noreply.github.com>
2025-12-15 18:30:46 +0000

    chore: added new API routes mainly ability to grab filters from search for use in presenting navigation options

--- services/api/api.py
+++ services/api/api.py
@@ -4,6 +4,7 @@ import json
 import psycopg2
 
 from tokeniser.assembler import Assembler
+from utilities.search import Search
 
 app = Flask(__name__)
 
@@ -17,6 +18,21 @@ def get_assembled_ref(translation, ref):
     scripture.add_detail()
     print(scripture.get_details())
     return jsonify(scripture.get_details())
+
+# Currently used as a way to present navigation options to user, by instantiationg disposable Search object
+@app.route("/info/scripture_filters")
+def get_scripture_params():
+    search = Search()
+    filters = search.get_filter()
+    return jsonify(filters)
+
+@app.route("/search/<word>")
+def search_word(word):
+    return f"Search for word: {word}"
+
+@app.route("/search/filter/<action>")
+def adjust_search_filter(action):
+    return f"Adjust search filter: {action}"
 
 if __name__ == "__main__":
     host_address = "localhost"
