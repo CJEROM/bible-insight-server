34cef5f789a1540af8e840cd6e3a8bbe9830de0c
CJEROM <118741301+CJEROM@users.noreply.github.com>
2025-12-15 18:03:35 +0000

    chore: added initial read api call to feed in assembled ref

--- services/api/api.py
+++ services/api/api.py
@@ -3,12 +3,23 @@ import sqlite3
 import json
 import psycopg2
 
+from tokeniser.assembler import Assembler
+
 app = Flask(__name__)
 
 @app.route("/init_database") 
 def get_data():
     return "Empty API Call"
 
+@app.route("/read/<translation>/<ref>")
+def get_assembled_ref(translation, ref):
+    scripture = Assembler(translation_id=translation, ref=ref)
+    scripture.add_detail()
+    print(scripture.get_details())
+    return jsonify(scripture.get_details())
+
 if __name__ == "__main__":
-    print("✅ Starting Flask server on http://localhost:5000 ...")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    host_address = "localhost"
+    host_port = 8000
+    print(f"✅ Starting Flask server on http://{host_address}:{host_port} ...")
+    app.run(host=host_address, port=host_port, debug=True, use_reloader=False)
