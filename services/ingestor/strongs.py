dfc02850e9647cea8e193469bdf7a8a46f406055
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-12-20 12:14:02 +0000

    chore: sorted script out so it now downlaods the xlsx file successfully

--- services/ingestor/strongs.py
+++ services/ingestor/strongs.py
@@ -7,17 +7,24 @@ SOURCE_URL = "https://dl.dropboxusercontent.com/scl/fi/pq1gsb2cf6n7hnp1l378d/Str
 # Depening on the source I use, the way I preprocess the data for my project will change
 class Strongs:
     def __init__(self):
-        self.strongs_csv_path = Path(__file__).parents[2] / "archive" / "strongs.xlsx"
+        self.strongs_csv_path = Path(__file__).parents[2] / "downloads" / "strongs.xlsx"
         if not self.strongs_csv_path.exists():
             self.download_strongs_csv()
 
     def download_strongs_csv(self):
         # In the future considering checking differences, and downloading based on that? if source requires
-        url = "https://www.dropbox.com/scl/fi/pq1gsb2cf6n7hnp1l378d/Strongs-Numbers.xlsx?dl=1"
+        url = SOURCE_URL
 
         self.strongs_csv_path.parent.mkdir(parents=True, exist_ok=True)
+        
+        with requests.get(url, stream=True, allow_redirects=True, timeout=60) as r:
+            r.raise_for_status()
+            with open(self.strongs_csv_path, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
 
-        r = requests.get(url, timeout=60)
-        r.raise_for_status()
+        print("Downloaded CSV file")
 
-        self.strongs_csv_path.write_bytes(r.content)
+if __name__ == "__main__":
+    Strongs()
