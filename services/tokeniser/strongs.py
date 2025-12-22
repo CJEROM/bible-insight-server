8047893b7fb95b11932df8c0c84016ae2fdf6f68
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2025-12-22 20:10:51 +0000

    chore: init Strongs Class

---
+++ services/tokeniser/strongs.py
@@ -0,0 +1,16 @@
+from manager.managerhandler import ManagerHandler
+
+class Strongs():
+    def __init__(self, manager: ManagerHandler):
+        self.manager = manager
+        self.db = manager.get_db()
+        
+        self.details = {
+            #
+        }
+
+    def search_strongs():
+        pass
+
+if __name__ == "__main":
+    pass
