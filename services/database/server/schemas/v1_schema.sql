f718abc01301070397917a81de791fee7d945111
CJEROM <118741301+CJEROM@users.noreply.github.com>
2025-12-22 06:58:49 +0000

    chore: adjusted strongs node reference to point to new lexemes table

--- services/database/server/schemas/v1_schema.sql
+++ services/database/server/schemas/v1_schema.sql
@@ -323,7 +323,7 @@ CREATE TABLE IF NOT EXISTS bible.nodes (
     FOREIGN KEY (book_map_id) REFERENCES bible.booktofile (id) ON DELETE CASCADE,
     FOREIGN KEY (translation_id) REFERENCES bible.translations (id) ON DELETE CASCADE,
     FOREIGN KEY (node_type) REFERENCES lookup.node_types (node) ON DELETE CASCADE,
-    FOREIGN KEY (strong) REFERENCES bible.strongs (code) ON DELETE SET NULL
+    FOREIGN KEY (strong) REFERENCES bible.lexemes (strongs_code) ON DELETE CASCADE
 );
 CREATE INDEX idx_bible_nodes_node_text ON bible.nodes (node_text) WHERE is_tokenisable = TRUE;
 CREATE INDEX idx_bible_nodes_sid ON bible.nodes (sid) WHERE sid IS NOT NULL;
