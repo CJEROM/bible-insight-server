cccd65d3307bb62022a07401e5bafbe0ddd50563
CJEROM <118741301+CJEROM@users.noreply.github.com>
2025-12-22 17:50:54 +0000

    chore: modularised download function to allow supplemental data downloads

--- services/ingestor/geoingestor.py
+++ services/ingestor/geoingestor.py
@@ -2,41 +2,59 @@ from pathlib import Path
 import requests
 import zipfile
 
-BASE_DIR = Path(__file__).parents[2] / "downloads" 
-BASE_DIR.mkdir(parents=True, exist_ok=True)
-
-ZIP_PATH = BASE_DIR / "Bible-Geocoding-Data.zip"
-EXTRACT_DIR = BASE_DIR 
-
 class GeoIngestor():
     def __init__(self):
-        self.download_geo_data()
+        # Download main Geo Data
+        self.download_data(
+            "https://github.com/openbibleinfo/Bible-Geocoding-Data/archive/refs/heads/main.zip",
+            "downloads" ,
+            "geodata",
+            "thumbnails.zip"
+        )
 
-    def download_geo_data(self):
-        # 
-        url = "https://github.com/openbibleinfo/Bible-Geocoding-Data/archive/refs/heads/main.zip"
+        # Download supplemental thumbnail images
+        self.download_data(
+            "https://a.openbible.info/geo/thumbnails.zip",
+            "downloads" ,
+            "geoimages",
+            "thumbnails.zip"
+        )
 
+        # ancient_json = 
+        # modern_json = 
+        # geometry_json = 
+        # image_json = 
+        # source_json = 
+
+    def download_data(self, url, base_folder, extract_folder, zip_file):
+        base_path = Path(__file__).parents[2] / base_folder
+        zip_path = base_path / zip_file
+        extract_path = base_path / extract_folder
+        
         response = requests.get(url, timeout=30)
         response.raise_for_status()
 
-        ZIP_PATH.write_bytes(response.content)
+        zip_path.write_bytes(response.content)
 
         # 4. Extract ZIP
-        EXTRACT_DIR.mkdir(parents=True, exist_ok=True)
+        extract_path.mkdir(parents=True, exist_ok=True)
 
-        with zipfile.ZipFile(ZIP_PATH) as z:
-            z.extractall(EXTRACT_DIR)
+        with zipfile.ZipFile(zip_path) as z:
+            z.extractall(extract_path)
 
         # 5. Locate extracted repo folder (GitHub always nests it)
         # e.g. Bible-Geocoding-Data-main/
-        repo_dir = next(EXTRACT_DIR.iterdir())
+        repo_dir = next(extract_path.iterdir())
 
-        print("ZIP saved at:      ", ZIP_PATH)
-        print("Extracted to:     ", EXTRACT_DIR)
+        print("ZIP saved at:      ", zip_path)
+        print("Extracted to:     ", extract_path)
         print("Repo root folder: ", repo_dir)
 
-        ZIP_PATH.unlink()  # delete zip after extraction
+        zip_path.unlink()  # delete zip after extraction
+        return extract_path
+
+    def ingest_jsonl(self, path):
+        pass
 
 if __name__ == "__main__":
     GeoIngestor()
-    pass
