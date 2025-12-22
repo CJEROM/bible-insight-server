8799731c6e350e94ee29d6fd7210b028b104a084
CJEROM <118741301+CJEROM@users.noreply.github.com>
2025-12-22 15:06:40 +0000

    chore: added modular function to extract strongs data and allow bulk insert into database

--- services/ingestor/strongsingestor.py
+++ services/ingestor/strongsingestor.py
@@ -5,9 +5,18 @@ import pandas
 from manager.managerhandler import ManagerHandler
 
 SOURCE_URL = "https://dl.dropboxusercontent.com/scl/fi/pq1gsb2cf6n7hnp1l378d/Strongs-Numbers.xlsx?rlkey=puni8uqrdgcbikq1fkqg52576&e=1"
+# See: https://www.ancient-hebrew.org/ahlb/aleph.html
 
 # Depening on the source I use, the way I preprocess the data for my project will change
 class StrongsIngestor:
+    SQL = {
+        "create_strongs": """
+            INSERT INTO bible.lexemes (source, strongs_code, language_id, lemma, raw_pos, transliteration, pronunciation, raw_gloss) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """,
+        "create_strongs_relation": """
+            INSERT INTO bible.lexeme_relations (from_lexeme, to_lexeme, relation_type, confidence, notes) VALUES (%s, %s, %s, %s, %s)
+        """,
+    }
     def __init__(self, manager: ManagerHandler):
         self.manager = manager
         self.log = manager.create_log_in_folder()
@@ -18,9 +27,15 @@ class StrongsIngestor:
         if not self.strongs_csv_path.exists():
             self.download_strongs_csv()
 
+        self.lexeme_mapping = {}
+        self.strongs_data = []
+        self.strongs_relations = []
+        self.lexeme_id = 1
+
         self.pre_process_xlsx()
         self.process_hebrew_sheet()
         self.process_greek_sheet()
+        self.bulk_export_strongs()
 
     def download_strongs_csv(self):
         # In the future considering checking differences, and downloading based on that? if source requires
@@ -69,6 +84,38 @@ class StrongsIngestor:
         print(sheet.columns)
         return sheet
 
+    def extract_strongs(self, sheet, language_id):
+        for row in sheet.itertuples(index=False):
+            this_lexeme = [None] * 8
+            this_lexeme[0] = "strongs"
+            strongs_code = f"G{row.number}"
+            this_lexeme[1] = strongs_code
+            this_lexeme[2] = language_id
+
+            if row.root != "":
+                this_lexeme[3] = row.root
+
+            this_lexeme[4] = row.part_of_speech
+
+            raw_gloss = ""
+
+            for i, line in enumerate(row.gloss.splitlines()):
+                if i == 0:  
+                    split_bracket = line.split("(")
+                    transliteration = split_bracket[0].strip()
+                    this_lexeme[5] = transliteration
+
+                    pronunciation = split_bracket[1].split(")")[0]
+                    this_lexeme[6] = pronunciation
+                elif not line.startswith("KJV") and not line.startswith("Root"):
+                    raw_gloss += f"{line}\n"
+            
+            this_lexeme[7] = raw_gloss
+            self.strongs_data.append(tuple(this_lexeme))
+            self.lexeme_mapping[strongs_code] = self.lexeme_id
+            # print(row.word)
+            self.lexeme_id+=1 # Increment every row
+
     def process_hebrew_sheet(self):
         sheet = self.get_sheet(
             "Hebrew",
@@ -80,32 +127,23 @@ class StrongsIngestor:
         language_id = self.db.fetch_clean_one("""
             SELECT id FROM bible.languages WHERE name LIKE 'Hebrew%'
         """)
-        for row in sheet.itertuples(index=False):
-            strongs_code = f"H{row.number}"
-            print(strongs_code)
+        self.extract_strongs(sheet, language_id)
 
     def process_greek_sheet(self):
         sheet = self.get_sheet(
             "Greek",
             ['#', 'Word', 'Gloss', 'Root',
-            'Occ in other words', 'prep1', 'prep2', 'R1', 'R1-Gk', 'R2', 'R2-Gk',
+            'prep1', 'prep2', 'R1', 'R1-Gk', 'R2', 'R2-Gk',
             'R3', 'R3-Gk', 'Part of Speech', 'cl.Heb.eqt.']
         )
         language_id = self.db.fetch_clean_one("""
             SELECT id FROM bible.languages WHERE name LIKE 'Greek%'
         """)
-        for row in sheet.itertuples(index=False):
-            # print(row.word)
-            pass
-        pass
+        self.extract_strongs(sheet, language_id)
 
-    def create_strongs(self):
-        # Write New Strongs entry to the database
-        pass
-
-    def create_strongs_relation(self):
-        # Write New Strongs Relation to the database e.g. from Root strongs etc.
-        pass
+    def bulk_export_strongs(self):
+        self.db.bulk_insert(self.SQL.get("create_strongs"), self.strongs_data)
+        # self.db.bulk_insert(self.SQL.get("create_strongs_relation"), self.strongs_relations)
 
 if __name__ == "__main__":
     StrongsIngestor()
