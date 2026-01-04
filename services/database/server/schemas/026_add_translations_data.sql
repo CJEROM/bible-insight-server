00e19bb96d72f2a1249449253f15656c47ae2d8c
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2026-01-04 04:50:39 +0000

    chore: further updates to schema separation

--- services/database/server/schemas/026_add_translations_data.sql
+++ services/database/server/schemas/026_add_translations_data.sql
@@ -1,18 +1,3 @@
-INSERT INTO users.readhistory (scripture_reference) VALUES 
-    ('GEN 1:1');
-
-INSERT INTO bible.languages (iso, name, nameLocal, scriptDirection) VALUES 
-    ('eng', 'English', 'English', 'LTR'),
-    ('hbo', 'Hebrew, Ancient', 'עברית', 'RTL'),
-    ('heb', 'Hebrew, Modern', 'עברית', 'RTL'),
-    ('grc', 'Greek, Ancient', 'Ελληνιστική', 'LTR');
-    -- ('', '', '', ''), 
-    -- Probably need to add the following (from dbl website - check whether language codes differ) 
-    --      [Greek: Ancient] [Ancient Greek (to 1453)] [Greek] [Hebrew] [Ancient Hebrew]
-    -- Greek : https://app.library.bible/content?languageIsoCodes=ell%2Cgrc%2Cgrc%2Cgrc&sortBy=name&sortOrder=asc [33]
-    -- Hebrew : https://app.library.bible/content?isOpenAccess=true&languageIsoCodes=heb%2Cheb&sortBy=name&sortOrder=asc [3]
-    -- English : https://app.library.bible/content?languageIsoCodes=ang%2Cbzj%2Ceng%2Cgul%2Cicr%2Cjam%2Cjam%2Clir&sortBy=name&sortOrder=asc [176]
-
 INSERT INTO bible.dblinfo (dbl_id, agreement_id) VALUES
     -- TEXT Medium Translations
     ('65eec8e0b60e656b', 246069), -- FBV | text | Free Bible Version
