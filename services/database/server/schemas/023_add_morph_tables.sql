51f783288e6fc4e8441674cd4ef53d107ad6511f
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2026-01-21 16:25:04 +0000

    feature: added rough initial linguistic data tables (subject to change on testing data ingestion)

---
+++ services/database/server/schemas/023_add_morph_tables.sql
@@ -0,0 +1,50 @@
+
+
+CREATE TABLE IF NOT EXISTS language.morph_scheme (
+    scheme_id        SERIAL PRIMARY KEY,
+    source_id        INTEGER,
+    code             TEXT,           -- e.g. STEP-GRK, Robinson-GRK
+    name             TEXT,
+    language         TEXT,           -- grc, heb
+    description      TEXT,
+    standard_ref     TEXT,           -- book/paper/spec
+    UNIQUE (source_id, code),
+    FOREIGN KEY (source_id) REFERENCES bible.sources(id),
+    FOREIGN KEY (language) REFERENCES language.languages(iso)
+)
+
+CREATE TABLE IF NOT EXISTS language.morph_key (
+    key_id           SERIAL PRIMARY KEY,
+    scheme_id        INTEGER,
+    key_code         TEXT,           -- tense, voice, gender
+    label            TEXT,           -- Tense
+    description      TEXT,
+    data_type        TEXT,           -- enum, text, numeric
+    sort_order       INT,
+    UNIQUE (scheme_id, key_code),
+    FOREIGN KEY (scheme_id) REFERENCES language.morph_scheme(scheme_id)
+)
+
+CREATE TABLE IF NOT EXISTS language.morph_value (
+    value_id         SERIAL PRIMARY KEY,
+    key_id           INTEGER,
+    value_code       TEXT,           -- A, P, Qal, Niphal
+    label            TEXT,           -- Aorist, Present, Qal
+    description      TEXT,
+    sort_order       INT,
+    UNIQUE (key_id, value_code),
+    FOREIGN KEY (key_id) REFERENCES language.morph_key(key_id),
+)
+
+CREATE TABLE IF NOT EXISTS language.token_morph_attr (
+    token_anchor_id  INTEGER,
+    scheme_id        INTEGER,
+    key_id           INTEGER,
+    value_id         INTEGER,
+    value_text       TEXT,           -- fallback for unmapped cases
+    PRIMARY KEY (token_anchor_id, scheme_id, key_id),
+    -- FOREIGN KEY (token_anchor_id) REFERENCES bible.tokens (token_id),
+    FOREIGN KEY (scheme_id) REFERENCES language.morph_scheme(scheme_id),
+    FOREIGN KEY (key_id) REFERENCES language.morph_key(key_id),
+    FOREIGN KEY (value_id) REFERENCES language.morph_value(value_id)
+)
