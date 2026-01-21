a99295e6c1f0d1d34df98623421e56089e1cef23
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2026-01-21 15:38:42 +0000

    chore: updated structural tables (chronology and harmony of scripture)

--- services/database/server/schemas/021_harmony_tables.sql
+++ services/database/server/schemas/021_harmony_tables.sql
@@ -1,11 +1,11 @@
 -- For parts of scripture talking about same period, and how it aligns e.g. Gospels
-CREATE TABLE IF NOT EXISTS bible.harmonies (
+CREATE TABLE IF NOT EXISTS structure.harmonies (
     id                  SERIAL PRIMARY KEY,
 	description			TEXT,
 	order_num			INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS bible.harmonymapping (
+CREATE TABLE IF NOT EXISTS structure.harmony_mapping (
     harmony_id          INTEGER,
 	verse_ref			TEXT,
 	PRIMARY KEY (harmony_id, verse_ref),
--- services/database/server/schemas/022_chronology_tables.sql
+++ services/database/server/schemas/022_chronology_tables.sql
@@ -1,12 +1,8 @@
 -- Order of all bible.verses in the bible, can also skip if concurrent sections are read as normal
-CREATE TABLE IF NOT EXISTS bible.chronologyoccurence (
+CREATE TABLE IF NOT EXISTS structure.chronology (
     id                  SERIAL PRIMARY KEY,
 	chapter_ref			TEXT,
-	verse_ref			TEXT
-);
-
-CREATE TABLE IF NOT EXISTS bible.chronology (
-    id                  SERIAL PRIMARY KEY,
-	prev_occurence		INTEGER,
-	FOREIGN KEY (prev_occurence) REFERENCES bible.chronologyoccurence (id)
+	verse_ref			TEXT,
+	order_num			INTEGER,
+	version				INTEGER
 );
