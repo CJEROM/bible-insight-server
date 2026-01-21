b004d9f165f83646ceebe70f9d5175b085c4245a
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2026-01-21 14:57:12 +0000

    chore: updated sources table to carry more metadata directly

--- services/database/server/schemas/003_reference_data_tables.sql
+++ services/database/server/schemas/003_reference_data_tables.sql
@@ -5,9 +5,16 @@ CREATE TABLE IF NOT EXISTS users.users (
 
 CREATE TABLE IF NOT EXISTS bible.sources (
     id                  SERIAL PRIMARY KEY,
-    url                 TEXT UNIQUE,
+    code                TEXT UNIQUE,   -- STEP, MorphGNT, OSHB
+    name                TEXT,
+    description         TEXT,
+    version             TEXT,
+    url                 TEXT,
 	note				TEXT,
-	metadata			JSONB
+    dateAccessed        TIMESTAMP,
+    parent_source       INTEGER,
+	metadata			JSONB,
+    FOREIGN KEY (parent_source) REFERENCES bible.sources(source_id)
 );
 CREATE INDEX idx_bible_sources_url ON bible.sources (url);
 
