4b5e31692a7b55f60696315a15df2e6364b176ee
Cephas Eromosele <118741301+CJEROM@users.noreply.github.com>
2026-01-21 15:38:18 +0000

    chore: more schemas added

--- services/database/server/schemas/001_schemas.sql
+++ services/database/server/schemas/001_schemas.sql
@@ -1,8 +1,12 @@
-CREATE SCHEMA bible;
-CREATE SCHEMA lookup;
-CREATE SCHEMA users;
-CREATE SCHEMA nlp;
-CREATE SCHEMA audit;
+CREATE SCHEMA bible;        -- canonical text & structure
+CREATE SCHEMA lookup;       -- enums, codes, standards
+CREATE SCHEMA users;        -- highlights, notes, activity
+CREATE SCHEMA nlp;          -- derived linguistic artifacts | applied annotations, inference, workflows
+CREATE SCHEMA audit;        -- system history
 
-CREATE SCHEMA language;
-CREATE SCHEMA entities;
+CREATE SCHEMA language;     -- linguistic systems & lexicons
+CREATE SCHEMA entity;       -- people, places, objects, events | asserted real-world things & relationships
+
+CREATE SCHEMA geo;          -- spatial data & mappings
+CREATE SCHEMA measure;      -- units, systems, conversions
+CREATE SCHEMA structure;    -- chronology, harmony, outlines
