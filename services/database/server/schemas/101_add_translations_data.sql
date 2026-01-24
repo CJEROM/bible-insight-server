7bf722f9b8fd5404dec13beb63bdcccb1bcb3a67
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-01-24 19:46:26 +0000

    chore: syntax changes fix (for tables I missed out earlier)

--- services/database/server/schemas/101_add_translations_data.sql
+++ services/database/server/schemas/101_add_translations_data.sql
@@ -35,5 +35,5 @@ INSERT INTO bible.dblinfo (dbl_id, agreement_id) VALUES
     -- ('',  ); --  | audio |
 
 INSERT INTO bible.dblinfo (dbl_id, agreement_id, supported, reason_not_supported) VALUES
-    ('ec290b5045ff54a5', 252291, FALSE, 'Not Valid -> Flawed USX files'), -- OKE | text | Targum Onkelos Etheridge
-    ('c89622d31b60c444', 272278, FALSE, 'Redundant -> introduces too much noise and not for our spec'); -- TOJB2011 | text | The Orthodox Jewish Bible
+    ('ec290b5045ff54a5', FALSE, 'Not Valid -> Flawed USX files'), -- OKE | text | Targum Onkelos Etheridge
+    ('c89622d31b60c444', FALSE, 'Redundant -> introduces too much noise and not for our spec'); -- TOJB2011 | text | The Orthodox Jewish Bible
