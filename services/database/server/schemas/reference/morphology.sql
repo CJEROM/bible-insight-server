18aeffbdad7ff85c1c61d96fdf5291e7bf3fecf8
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-01-28 21:45:35 +0000

    chore: correceted morphology table syntax and primary key names and references

--- services/database/server/schemas/reference/morphology.sql
+++ services/database/server/schemas/reference/morphology.sql
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE IF NOT EXISTS language.morph_scheme (
-    scheme_id        SERIAL PRIMARY KEY,
+    id        SERIAL PRIMARY KEY,
     source_id        INTEGER,
     code             TEXT,           -- e.g. STEP-GRK, Robinson-GRK
     name             TEXT,
@@ -11,10 +11,10 @@ CREATE TABLE IF NOT EXISTS language.morph_scheme (
     UNIQUE (source_id, code),
     FOREIGN KEY (source_id) REFERENCES audit.sources(id),
     FOREIGN KEY (language) REFERENCES language.languages(iso)
-)
+);
 
 CREATE TABLE IF NOT EXISTS language.morph_key (
-    key_id           SERIAL PRIMARY KEY,
+    id           SERIAL PRIMARY KEY,
     scheme_id        INTEGER,
     key_code         TEXT,           -- tense, voice, gender
     label            TEXT,           -- Tense
@@ -22,19 +22,19 @@ CREATE TABLE IF NOT EXISTS language.morph_key (
     data_type        TEXT,           -- enum, text, numeric
     sort_order       INT,
     UNIQUE (scheme_id, key_code),
-    FOREIGN KEY (scheme_id) REFERENCES language.morph_scheme(scheme_id)
-)
+    FOREIGN KEY (scheme_id) REFERENCES language.morph_scheme(id)
+);
 
 CREATE TABLE IF NOT EXISTS language.morph_value (
-    value_id         SERIAL PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     key_id           INTEGER,
     value_code       TEXT,           -- A, P, Qal, Niphal
     label            TEXT,           -- Aorist, Present, Qal
     description      TEXT,
     sort_order       INT,
     UNIQUE (key_id, value_code),
-    FOREIGN KEY (key_id) REFERENCES language.morph_key(key_id),
-)
+    FOREIGN KEY (key_id) REFERENCES language.morph_key(id)
+);
 
 CREATE TABLE IF NOT EXISTS language.token_morph_attr (
     token_anchor_id  INTEGER,
@@ -46,5 +46,5 @@ CREATE TABLE IF NOT EXISTS language.token_morph_attr (
     -- FOREIGN KEY (token_anchor_id) REFERENCES bible.tokens (token_id),
     FOREIGN KEY (scheme_id) REFERENCES language.morph_scheme(scheme_id),
     FOREIGN KEY (key_id) REFERENCES language.morph_key(key_id),
-    FOREIGN KEY (value_id) REFERENCES language.morph_value(value_id)
-)
+    FOREIGN KEY (value_id) REFERENCES language.morph_value(id)
+);
