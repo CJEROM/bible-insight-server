20042de945c0f5dd9e9fe2293b99a05f5cdf4a28
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-01-29 02:09:56 +0000

    feature: new table to represent languages script for better support of ancient lettering e.g. pictograms

--- services/database/server/schemas/reference/languages.sql
+++ services/database/server/schemas/reference/languages.sql
@@ -7,15 +7,40 @@ CREATE TABLE IF NOT EXISTS language.languages (
 );
 CREATE INDEX idx_bible_languages_iso ON language.languages (iso);
 
+CREATE TABLE IF NOT EXISTS language.scripts (
+    id              SERIAL PRIMARY KEY,
+
+    name            TEXT NOT NULL,        -- Paleo-Hebrew, Square Hebrew
+    description     TEXT,
+    period          TEXT,                 -- Optional textual period label
+    script_type     TEXT,                 -- pictographic, abjad, alphabet
+    
+    language_id     INTEGER NOT NULL,
+    source_id       INTEGER,
+    is_default      BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (language_id) REFERENCES language.languages (id),
+    FOREIGN KEY (source_id) REFERENCES audit.sources (id)
+);
+
+-- Potentially 
 CREATE TABLE IF NOT EXISTS language.language_letters (
     id              SERIAL PRIMARY KEY,
+
+    name            TEXT,
+    description     TEXT,
+    meaning         TEXT,       -- OR definition
     letter          TEXT,  
-    phoneme         TEXT, -- Whether 'vowel' or 'consonant'
-    parent_letter   INTEGER,
-    ancient_symbol  INTEGER,
-    symbol_def      TEXT,
-    language_id     INTEGER,
+
+    ancient_symbol  INTEGER,    -- File ID to SVG of symbol
+    phoneme         TEXT,       -- IPA representation (letter sound) -> Move to separate table for multiple phonemes?
+    letter_type     TEXT,       -- 'vowel' or 'consonant'
+
+    parent_letter   INTEGER,    -- For linking variants to main
+    script_id       INTEGER NOT NULL,
+    source_id       INTEGER,
+
     FOREIGN KEY (parent_letter) REFERENCES language.language_letters (id),
     FOREIGN KEY (ancient_symbol) REFERENCES audit.files (id),
-    FOREIGN KEY (language_id) REFERENCES language.languages (id)
+    FOREIGN KEY (script_id) REFERENCES language.scripts (id),
+    FOREIGN KEY (source_id) REFERENCES audit.sources (id)
 );
