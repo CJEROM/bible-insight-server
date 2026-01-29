e99129a75066b7e9bef60e35e4aec84c58b78311
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-01-29 02:24:38 +0000

    chore: corrected seed data

--- services/database/server/seed/translation_data.sql
+++ services/database/server/seed/translation_data.sql
@@ -34,6 +34,6 @@
 --     -- Audio Medium Translations
 --     -- ('',  ); --  | audio |
 
-INSERT INTO audit.dbl_info (dbl_id, agreement_id, supported, reason_not_supported) VALUES
+INSERT INTO audit.dbl_info (dbl_id, supported, reason_not_supported) VALUES
     ('ec290b5045ff54a5', FALSE, 'Not Valid -> Flawed USX files'), -- OKE | text | Targum Onkelos Etheridge
     ('c89622d31b60c444', FALSE, 'Redundant -> introduces too much noise and not for our spec'); -- TOJB2011 | text | The Orthodox Jewish Bible
