99e72010230f28b3463c90e5adfc526bbaf4fdd3
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-02-03 20:07:39 +0000

    chore: created LDML Class as placeholder, currently not in use (might be required for NLP pipelines but not right now)

---
+++ services/ingestor/usx/files/ldml.py
@@ -0,0 +1,7 @@
+from base_file import BaseFile
+
+class LDML(BaseFile):
+    def __init__(self):
+        # Currently no functionality derived from it, but we may use it for improving NLP
+        #   Clean up so that tokenisation can be more accurate.
+        pass
