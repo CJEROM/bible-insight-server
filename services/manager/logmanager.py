f8771e37dd2414c1228448d057650052555f9d04
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-02-06 18:55:25 +0000

    chore: improved logging capabilities in log manager, now can programmtically parse logs as Tab separated files

--- services/manager/logmanager.py
+++ services/manager/logmanager.py
@@ -197,7 +197,13 @@ class LogManager():
             return
 
         with open(self.log_file, 'a', encoding="utf-8") as f:
-            f.write(f"{datetime.datetime.now()} [{log_level}] [Elapsed: {self.elapsed_time()}] [{source_class}] {log_message}\n")
+            f.write(
+                f"{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]:<23}\t"
+                f"[{log_level:<5}]\t"
+                f"[Elapsed: {self.elapsed_time():<8}]\t"
+                f"[{source_class:<15}]\t"
+                f"{log_message}\n"
+            )
 
 if __name__ == "__main__":
     pass
