f2334ecda32da1b61a9acab976679f196375ddc3
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-02-07 18:37:02 +0000

    feature: BIL-39: added new functions to search a file by to base read boundaryy

--- services/database/boundary/base_boundary.py
+++ services/database/boundary/base_boundary.py
@@ -15,6 +15,44 @@ class ReadBoundary(QueryBoundary):
         """
         results = self.db.fetch_one(query, (file_id, ))
         return results
+    
+    def get_latest_file(self,
+            bucket      : str,
+            object_path : str
+        ) -> int:
+        query = """
+            SELECT id
+            FROM audit.files
+            WHERE bucket = %s
+                AND object_path = %s
+            ORDER BY import_time DESC
+            LIMIT 1;
+        """
+        file_id = self.db.fetch_clean_one(query, (bucket, object_path))
+        return file_id
+    
+    def get_file_by_hash(self,
+            content_hash    : str,
+            data_format     : str
+        ) -> int:
+        # Hashes
+        #       - Same content → same hash
+        #       - Different content → different hash (for all practical purposes)
+        #       - Did this file change? = Have these exact bytes ever existed in my system before?
+  
+        # Try exact content match
+        query_hash = """
+            SELECT id
+            FROM audit.files
+            WHERE content_hash = %s
+                AND data_format = %s
+            LIMIT 1;
+        """
+        file_id = self.db.fetch_clean_one(
+            query_hash,
+            (content_hash, data_format)
+        )
+        return file_id
 
 class WriteBoundary(QueryBoundary):
     pass
