c1c1f73f57ab9ddb5c69e75fb5850c05d8fd2bd7
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-02-08 00:11:07 +0000

    fix: BIL-84: corrected structure for the schema

--- services/database/server/schemas/sil/language_codes.sql
+++ services/database/server/schemas/sil/language_codes.sql
@@ -5,20 +5,20 @@
 -- ============================================================================
 
 CREATE TABLE sil.iso_codes (
-    id          char(3) NOT NULL,  -- The three-letter 639-3 identifier
-    part2b      char(3) NULL,      -- Equivalent 639-2 identifier of the bibliographic applications (if there is one)
-    part2t      char(3) NULL,      -- Equivalent 639-2 identifier of the terminology applications code (if there is one)
-    part1       char(2) NULL,      -- Equivalent 639-1 identifier, (if there is one)
+    id          char(3) NOT NULL,       -- The three-letter 639-3 identifier
+    part2b      char(3) NULL,           -- Equivalent 639-2 identifier of the bibliographic applications (if there is one)
+    part2t      char(3) NULL,           -- Equivalent 639-2 identifier of the terminology applications code (if there is one)
+    part1       char(2) NULL,           -- Equivalent 639-1 identifier, (if there is one)
     scope       char(1) NOT NULL, 
     type        char(1) NOT NULL,
-    ref_name    varchar(150) NOT NULL,   -- Reference language name 
-    comment     varchar(150) NULL,       -- Comment relating to one or more of the columns
+    ref_name    varchar(150) NOT NULL,  -- Reference language name 
+    comment     varchar(150) NULL,      -- Comment relating to one or more of the columns
     FOREIGN KEY (scope) REFERENCES sil.iso_scopes (id),
     FOREIGN KEY (type) REFERENCES sil.iso_types (id)
 );
 
-CREATE TABLE sil.scopes (
-    id          TEXT PRIMARY KEY,
+CREATE TABLE sil.iso_scopes (
+    id          char(1) PRIMARY KEY,
     name        TEXT NOT NULL,
     description TEXT
 );
@@ -30,12 +30,12 @@ VALUES
     ('S', 'Special', '');
 
 CREATE TABLE sil.iso_types (
-    id          TEXT PRIMARY KEY,
+    id          char(1) PRIMARY KEY,
     name        TEXT NOT NULL,
     description TEXT
 );
 
-INSERT VALUES INTO sil.types (code, name, description)
+INSERT VALUES INTO sil.iso_types (code, name, description)
 VALUES 
     ('A', 'Ancient', ''),
     ('C', 'Constructed', ''),
@@ -49,9 +49,10 @@ VALUES
 -- ============================================================================
 
 CREATE TABLE sil.iso_names (
-    iso_code        char(3)     PRIMARY KEY,    -- The three-letter 639-3 identifier
-    print_name      varchar(75) NOT NULL,       -- One of the names associated with this identifier 
-    inverted_name   varchar(75) NOT NULL,       -- The inverted form of this Print_Name form   
+    id              SERIAL PRIMARY KEY,
+    iso_code        char(3)     NOT NULL,   -- The three-letter 639-3 identifier
+    print_name      varchar(75) NOT NULL,   -- One of the names associated with this identifier 
+    inverted_name   varchar(75) NOT NULL,   -- The inverted form of this Print_Name form   
     FOREIGN KEY (iso_code) REFERENCES sil.iso_codes (id)
 ); 
 
@@ -60,15 +61,16 @@ CREATE TABLE sil.iso_names (
 -- ============================================================================
 
 CREATE TABLE sil.macrolanguages (
-    macro_id            char(3) PRIMARY KEY,    -- The identifier for a macrolanguage
-    iso_id              char(3) NOT NULL,       -- The identifier for an individual language that is a member of the macrolanguage
-    iso_status          char(1) NOT NULL,       -- A (active) or R (retired) indicating the status of the individual code element
+    macro_id            char(3) NOT NULL,   -- The identifier for a macrolanguage
+    iso_id              char(3) NOT NULL,   -- The identifier for an individual language that is a member of the macrolanguage
+    iso_status          char(1) NOT NULL,   -- indicating the status of the individual code element
+    PRIMARY KEY (macro_id, iso_id)
     FOREIGN KEY (iso_id) REFERENCES sil.iso_codes (id),
     FOREIGN KEY (iso_status) REFERENCES sil.macro_status (id)
 );
 
 CREATE TABLE sil.macro_status (
-    id          TEXT PRIMARY KEY,
+    id          char(1) PRIMARY KEY,
     name        TEXT NOT NULL,
     description TEXT
 );
@@ -92,11 +94,12 @@ CREATE TABLE sil.retirements (
     Effective           DATE        NOT NULL,       -- The date the retirement became effective
     FOREIGN KEY (iso_code) REFERENCES sil.iso_codes (id),
     FOREIGN KEY (retired_reason) REFERENCES sil.retirement_reasons (id),
+    FOREIGN KEY (changed_to) REFERENCES sil.iso_codes (id),
     -- FOREIGN KEY (changed_to) REFERENCES sil.iso_codes (id),
 );
 
 CREATE TABLE lookup.retirement_reasons (
-    id              TEXT PRIMARY KEY,
+    id              char(1) PRIMARY KEY,
     name            TEXT NOT NULL,
     description     TEXT
 )
