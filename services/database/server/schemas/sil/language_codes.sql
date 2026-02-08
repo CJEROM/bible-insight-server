473d12666036c447c72a5bec48148c19bd496674
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-02-08 02:45:51 +0000

    chore: BIL-84: more corrections

--- services/database/server/schemas/sil/language_codes.sql
+++ services/database/server/schemas/sil/language_codes.sql
@@ -5,7 +5,7 @@
 -- ============================================================================
 
 CREATE TABLE sil.iso_codes (
-    id          char(3) NOT NULL,       -- The three-letter 639-3 identifier
+    id          char(3) PRIMARY KEY,    -- The three-letter 639-3 identifier
     part2b      char(3) NULL,           -- Equivalent 639-2 identifier of the bibliographic applications (if there is one)
     part2t      char(3) NULL,           -- Equivalent 639-2 identifier of the terminology applications code (if there is one)
     part1       char(2) NULL,           -- Equivalent 639-1 identifier, (if there is one)
@@ -64,7 +64,7 @@ CREATE TABLE sil.macrolanguages (
     macro_id            char(3) NOT NULL,   -- The identifier for a macrolanguage
     iso_id              char(3) NOT NULL,   -- The identifier for an individual language that is a member of the macrolanguage
     iso_status          char(1) NOT NULL,   -- indicating the status of the individual code element
-    PRIMARY KEY (macro_id, iso_id)
+    PRIMARY KEY (macro_id, iso_id),
     FOREIGN KEY (iso_id) REFERENCES sil.iso_codes (id),
     FOREIGN KEY (iso_status) REFERENCES sil.macro_status (id)
 );
@@ -97,7 +97,7 @@ CREATE TABLE sil.retirements (
     FOREIGN KEY (changed_to) REFERENCES sil.iso_codes (id)
 );
 
-CREATE TABLE lookup.retirement_reasons (
+CREATE TABLE sil.retirement_reasons (
     id              char(1) PRIMARY KEY,
     name            TEXT NOT NULL,
     description     TEXT NOT NULL
