cd22a15600b16b27617a2dcad5df6a61de5fcae3
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-02-10 17:50:43 +0000

    chore: BIL-89: preparing for SIL Ingestion through setting up module and Query Boundary for ingestion

---
+++ services/database/boundary/sil_boundary.py
@@ -0,0 +1,10 @@
+from database.boundary.base_boundary import ReadBoundary, WriteBoundary, DeleteBoundary
+
+class SILReadBoundary(ReadBoundary):
+    pass
+
+class SILWriteBoundary(WriteBoundary):
+    pass
+
+class SILDeleteBoundary(DeleteBoundary):
+    pass
