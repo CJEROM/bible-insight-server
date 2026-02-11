34e9d07b4a45cb36da84bdcfd4b45ab46e6520ba
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-02-11 15:12:20 +0000

    chore: BIL-89: completed import processing of main ISO 639-3 language codes file

--- services/ingestor/sil/iso6393_codes.py
+++ services/ingestor/sil/iso6393_codes.py
@@ -23,6 +23,7 @@ class ISO6393Codes(BaseFile):
         self.read_file()
 
     def read_file(self):
+        added_codes = 0
         with open(self.this_file_path, "r", encoding="utf-8") as f:
             # ['Id', 'Part2b', 'Part2t', 'Part1', 'Scope', 'Language_Type', 'Ref_Name', 'Comment']
             header = next(f).rstrip("\n").split("\t")
@@ -31,8 +32,21 @@ class ISO6393Codes(BaseFile):
 
             for line in f:
                 columns = line.rstrip("\n").split("\t")
-                # print(columns)
-                # columns is now a list of values
+
+                self.write.persist_iso_code(
+                    iso_code    = columns[0], # Id
+                    part2b      = columns[1], # Part2b
+                    part2t      = columns[2], # Part2t
+                    part1       = columns[3], # Part1
+                    scope       = columns[4], # Scope
+                    type        = columns[5], # Language_Type
+                    ref_name    = columns[6], # Ref_Name
+                    comment     = columns[7]  # Comment
+                )
+                added_codes += 1
+        
+        print(f"Added [{added_codes}] New ISO 639-3 Codes")
+
                 
 if __name__ == "__main__":
     from manager.managerhandler import ManagerHandler
