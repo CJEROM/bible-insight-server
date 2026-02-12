b4c32ce3a6b38a16d9b1fb290775965092a6d4cf
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-02-12 00:13:09 +0000

    chore: BIL-89: set 3rd re read of the file to allow for 1. iso_codes (updated) 2. retirements themselves 3. retirement changes mapping

--- services/ingestor/sil/iso6393_retirements.py
+++ services/ingestor/sil/iso6393_retirements.py
@@ -37,16 +37,6 @@ class ISO6393Retirements(BaseFile):
                 # Tab separated values -> Array
                 columns = line.rstrip("\n").split("\t")
 
-                # Persist to database
-                retirement_id = self.write.persist_iso_retirements(
-                    iso_code        = columns[0], # Id
-                    ref_name        = columns[1], # Ref_Name
-                    retired_reason  = columns[2], # Ret_Reason
-                    retired_remedy  = columns[4], # Ret_Remedy
-                    effective       = columns[5]  # Effective
-                )
-                added_retirements += 1
-
                 self.write.persist_iso_code(
                     iso_code    = columns[0],   # Id
                     part2b      = None,         # Part2b
@@ -59,11 +49,30 @@ class ISO6393Retirements(BaseFile):
                     comment     = 'DERIVED -> FROM Retirements'  # Comment
                 )
                 added_iso_codes += 1
+
+        with open(self.this_file_path, "r", encoding="utf-8") as f:
+            # ['Id', 'Ref_Name', 'Ret_Reason', 'Change_To', 'Ret_Remedy', 'Effective']
+            header = next(f).rstrip("\n").split("\t") # Skip Header line
+
+            # Go through file line by line
+            for line in f:
+                # Tab separated values -> Array
+                columns = line.rstrip("\n").split("\t")
+
+                # Persist to database
+                retirement_id = self.write.persist_iso_retirements(
+                    iso_code        = columns[0], # Id
+                    ref_name        = columns[1], # Ref_Name
+                    retired_reason  = columns[2], # Ret_Reason
+                    retired_remedy  = columns[4], # Ret_Remedy
+                    effective       = columns[5]  # Effective
+                )
+                added_retirements += 1
         
         with open(self.this_file_path, "r", encoding="utf-8") as f:
             # ['Id', 'Ref_Name', 'Ret_Reason', 'Change_To', 'Ret_Remedy', 'Effective']
             header = next(f).rstrip("\n").split("\t") # Skip Header line
-            
+
             # Go through file line by line
             for line in f:
                 # Tab separated values -> Array
@@ -71,32 +80,40 @@ class ISO6393Retirements(BaseFile):
 
                 retirement_id = None
 
-                # Get retirement to link to first
-                if (not self.read.is_iso_code(columns[0])):
-                    retirement_id = self.read.get_retirement(columns[0])
-
-                print(retirement_id)
-                print(columns)
-
                 # Change codes mapped to retirement_id
                 if columns[3] != "":
-                    
-                    self.write.persist_iso_retirement_changes(
-                        retirement_id   = retirement_id,
-                        changed_to      = columns[3] # Change_To
-                    )
-                    added_change_codes += 1
+                    # Get retirement to link to first
+                    retirement_id = self.read.get_retirement(columns[0])
+                
+                    if retirement_id != None:
+                        print(retirement_id)
+
+                        self.write.persist_iso_retirement_changes(
+                            retirement_id   = retirement_id,
+                            changed_to      = columns[3] # Change_To
+                        )
+                        added_change_codes += 1
+                    else:
+                        print(columns)
+                        pass
 
                 # In the case of Ret_Reason 'S' - 'Split' it has all the new codes that the retired has been chaged to
                 #       within 
                 if columns[2] == 'S':
                     change_codes = re.findall(r"\[([^\]]+)\]", columns[4]) # Ret_Remedy
                     for code in change_codes:
-                        self.write.persist_iso_retirement_changes(
-                            retirement_id   = retirement_id,
-                            changed_to      = code
-                        )
-                        added_change_codes += 1
+                        # Get retirement to link to first
+                        retirement_id = self.read.get_retirement(code)
+
+                        if retirement_id != None:
+                            print(retirement_id)
+                            self.write.persist_iso_retirement_changes(
+                                retirement_id   = retirement_id,
+                                changed_to      = code
+                            )
+                            added_change_codes += 1
+                        else:
+                            print(columns)
             
         print(f"Added [{added_retirements}] Retirements + [{added_change_codes}] Change Codes + [{added_iso_codes}] Retired ISO Codes")
                 
