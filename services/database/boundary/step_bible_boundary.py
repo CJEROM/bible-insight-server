66c57c08d54540f1242a415dc7d7a2a65487ff5f
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-03-08 09:43:46 +0000

    chore: created new query boundary for new STEP Bible data Ingestion

---
+++ services/database/boundary/step_bible_boundary.py
@@ -0,0 +1,10 @@
+from database.boundary.base_boundary import ReadBoundary, WriteBoundary, DeleteBoundary
+
+class StepBibleReadBoundary(ReadBoundary):
+    pass    
+
+class StepBibleWriteBoundary(WriteBoundary):
+    pass    
+
+class StepBibleDeleteBoundary(DeleteBoundary):
+    pass
