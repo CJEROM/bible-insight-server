2e5b216df6b224e6ff87c03795b613555a29404f
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-03-14 04:51:56 +0000

    chore: organised classes in more distinct folders for stepbible files, to abstract the areas they are relevant to easier

--- services/ingestor/stepbible/ingestor.py
+++ services/ingestor/stepbible/ingestor.py
@@ -1,16 +1,19 @@
 
 
 
-from ingestor.stepbible.TAHOT import TAHOT
-from ingestor.stepbible.TAGNT import TAGNT
-from ingestor.stepbible.TEBSG import TEBSG
-from ingestor.stepbible.TEBSH import TEBSH
-from ingestor.stepbible.TEGMC import TEGMC
-from ingestor.stepbible.TEHMC import TEHMC
-from ingestor.stepbible.TFLSJ import TFLSJ
-from ingestor.stepbible.TIPNR import TIPNR
-from ingestor.stepbible.TTESV import TTESV
-from ingestor.stepbible.TVTMS import TVTMS
+from services.ingestor.stepbible.tagged.TAHOT import TAHOT
+from services.ingestor.stepbible.tagged.TAGNT import TAGNT
+from services.ingestor.stepbible.tagged.TTESV import TTESV
+
+from services.ingestor.stepbible.lexicon.TEBSG import TEBSG
+from services.ingestor.stepbible.lexicon.TEBSH import TEBSH
+from services.ingestor.stepbible.lexicon.TFLSJ import TFLSJ
+
+from services.ingestor.stepbible.morphology.TEGMC import TEGMC
+from services.ingestor.stepbible.morphology.TEHMC import TEHMC
+
+from services.ingestor.stepbible.other.TIPNR import TIPNR
+from services.ingestor.stepbible.other.TVTMS import TVTMS
 
 from manager.managerhandler import ManagerHandler
 from database.boundary.step_bible_boundary import StepBibleReadBoundary, StepBibleWriteBoundary, StepBibleDeleteBoundary
@@ -42,66 +45,66 @@ class STEPBibleIngestor:
             log         = self.log
         )
 
-        # ================================================================================================================
-        #                                                   Lexicons
-        # ================================================================================================================
+        # # ================================================================================================================
+        # #                                                   Lexicons
+        # # ================================================================================================================
 
-        # Translators Brief lexicon of Extended Strongs for Greek
-        TEBSG(
-            manager     = self.manager,
-            log         = self.log
-        )
-        # Translators Brief lexicon of Extended Strongs for Hebrew
-        TEBSH(
-            manager     = self.manager,
-            log         = self.log
-        )
-        # Translators Formatted full LSJ Bible lexicon
-        TFLSJ(
-            manager     = self.manager,
-            log         = self.log
-        )
+        # # Translators Brief lexicon of Extended Strongs for Greek
+        # TEBSG(
+        #     manager     = self.manager,
+        #     log         = self.log
+        # )
+        # # Translators Brief lexicon of Extended Strongs for Hebrew
+        # TEBSH(
+        #     manager     = self.manager,
+        #     log         = self.log
+        # )
+        # # Translators Formatted full LSJ Bible lexicon
+        # TFLSJ(
+        #     manager     = self.manager,
+        #     log         = self.log
+        # )
 
-        # ================================================================================================================
-        #                                                   Tagged Bibles
-        # ================================================================================================================
+        # # ================================================================================================================
+        # #                                                   Tagged Bibles
+        # # ================================================================================================================
 
-        # Translators Amalgamated Hebrew OT
-        TAHOT(
-            manager     = self.manager,
-            log         = self.log
-        )
-        # Translators Amalgamated Greek NT
-        TAGNT(
-            manager     = self.manager,
-            log         = self.log
-        )
-        # Tyndale Translation tags for ESV
-        TTESV(
-            manager     = self.manager,
-            log         = self.log
-        )
+        # # Translators Amalgamated Hebrew OT
+        # TAHOT(
+        #     manager     = self.manager,
+        #     log         = self.log
+        # )
+        # # Translators Amalgamated Greek NT
+        # TAGNT(
+        #     manager     = self.manager,
+        #     log         = self.log
+        # )
+        # # Tyndale Translation tags for ESV
+        # TTESV(
+        #     manager     = self.manager,
+        #     log         = self.log
+        # )
 
-        # ================================================================================================================
-        #                                                   Entities
-        # ================================================================================================================
+        # # ================================================================================================================
+        # #                                                   Entities
+        # # ================================================================================================================
         
-        # Translators Individualised Proper Names with all References
-        TIPNR(
-            manager     = self.manager,
-            log         = self.log
-        )
+        # # Translators Individualised Proper Names with all References
+        # TIPNR(
+        #     manager     = self.manager,
+        #     log         = self.log
+        # )
 
-        # ================================================================================================================
-        #                                                   Versification
-        # ================================================================================================================
+        # # ================================================================================================================
+        # #                                                   Versification
+        # # ================================================================================================================
         
-        # Translators Versification Traditions with Methodology 
-        #       for Standardisation for Eng+Heb+Lat+Grk+Others
-        TVTMS(
-            manager     = self.manager,
-            log         = self.log
-        )
+        # # Translators Versification Traditions with Methodology 
+        # #       for Standardisation for Eng+Heb+Lat+Grk+Others
+        # TVTMS(
+        #     manager     = self.manager,
+        #     log         = self.log
+        # )
 
 if __name__ == "__main__":
     manager = ManagerHandler()
