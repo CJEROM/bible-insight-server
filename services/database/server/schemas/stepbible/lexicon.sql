7b971e7a019991dbc4c875b6dedeb131caf93f55
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-03-15 05:23:39 +0000

    chore: update lexicon sql script with db schema with corrections

--- services/database/server/schemas/stepbible/lexicon.sql
+++ services/database/server/schemas/stepbible/lexicon.sql
@@ -1,118 +1,138 @@
-
-DROP SCHEMA lexicon CASCADE;
+DROP SCHEMA IF EXISTS lexicon CASCADE;
 CREATE SCHEMA lexicon;
 
 -- ==============================================================================================================================
+-- LANGUAGE
+-- ==============================================================================================================================
 
 CREATE TABLE IF NOT EXISTS lexicon.language(
-    id          CHAR(1) PRIMARY KEY,
-    name        TEXT,
-    iso         CHAR(3),
-    FOREIGN KEY (iso) REFERENCES standards.iso693_3_codes (id)
+    id      TEXT PRIMARY KEY,
+    name    TEXT,
+    iso     CHAR(3),
+    FOREIGN KEY (iso) REFERENCES standards.iso639_3_codes (id)
 );
--- Language is A=Aramaic, H=Hebrew, G=Greek and N=Name (which is not language specific). 
+
+-- Language is A=Aramaic, H=Hebrew, G=Greek and N=Name (not language specific)
 INSERT INTO lexicon.language (id, name, iso)
 VALUES
-    ("A", "Aramaic" , "arc"),
-    ("H", "Hebrew"  , "hbo"), -- Ancient Hebrew / 'heb' -> Modern Hebrew
-    ("G", "Greek"   , "grc"),
-    ("N", "Name"    , None);
+    ('A', 'Aramaic' , 'arc'),
+    ('H', 'Hebrew'  , 'hbo'), -- Ancient Hebrew
+    ('G', 'Greek'   , 'grc'),
+    ('N', 'Name'    , NULL);
+
+-- ==============================================================================================================================
+-- TYPE (PART OF SPEECH)
+-- ==============================================================================================================================
 
 CREATE TABLE IF NOT EXISTS lexicon.type(
-    id          CHAR(1) PRIMARY KEY,
-    name        TEXT
+    id      TEXT PRIMARY KEY,
+    name    TEXT
 );
--- Type is A=Adjective, Adv=Adverb, Art=Article, Cond=Conditional, Conj=Conjunction, Cor=Correlative, DemP=Demonstrative Pn, ImpP=Impersonal Pn, Intg=Interogative, Intj=Interjection, N=Noun, Neg=Negative, Part=Particle, Prep=Preposition, PerP=Personal Pn, PosP=Possessive Pn, RefP=Reflexive Pn, RelP=Relative Pn, V=Verb. 
+
+-- Type is A=Adjective, Adv=Adverb, Art=Article, etc.
 INSERT INTO lexicon.type (id, name)
 VALUES
-    ("A"    , "Adjective"),
-    ("Adv"  , "Adverb"),
-    ("Art"  , "Article"),
-    ("Cond" , "Conditional"),
-    ("Conj" , "Conjunction"),
-    ("Cor"  , "Correlative"),
-    ("DemP" , "Demonstrative Pn"),
-    ("ImpP" , "Impersonal Pn"),
-    ("Intg" , "Interogative"),
-    ("Intj" , "Interjection"),
-    ("N"    , "Noun"),
-    ("Neg"  , "Negative"),
-    ("Part" , "Particle"),
-    ("Prep" , "Preposition"),
-    ("PerP" , "Personal Pn"),
-    ("PosP" , "Possessive Pn"),
-    ("RefP" , "Reflexive Pn"),
-    ("RelP" , "Relative Pn"),
-    ("V"    , "Verb");
+    ('A'    , 'Adjective'),
+    ('Adv'  , 'Adverb'),
+    ('Art'  , 'Article'),
+    ('Cond' , 'Conditional'),
+    ('Conj' , 'Conjunction'),
+    ('Cor'  , 'Correlative'),
+    ('DemP' , 'Demonstrative Pronoun'),
+    ('ImpP' , 'Impersonal Pronoun'),
+    ('Intg' , 'Interrogative'),
+    ('Intj' , 'Interjection'),
+    ('N'    , 'Noun'),
+    ('Neg'  , 'Negative'),
+    ('Part' , 'Particle'),
+    ('Prep' , 'Preposition'),
+    ('PerP' , 'Personal Pronoun'),
+    ('PosP' , 'Possessive Pronoun'),
+    ('RefP' , 'Reflexive Pronoun'),
+    ('RelP' , 'Relative Pronoun'),
+    ('V'    , 'Verb');
+
+-- ==============================================================================================================================
+-- GENDER
+-- ==============================================================================================================================
 
 CREATE TABLE IF NOT EXISTS lexicon.gender(
-    id          CHAR(1) PRIMARY KEY,
-    name        TEXT
+    id      TEXT PRIMARY KEY,
+    name    TEXT
 );
--- Gender is F=Female, M=Male, N=Neuter, C=Common and P or S is optionally added for Plural Singular
+
+-- Gender is F=Female, M=Male, N=Neuter, C=Common, P=Plural, S=Singular
 INSERT INTO lexicon.gender (id, name)
 VALUES
-    ("F", "Female"),
-    ("M", "Male"), 
-    ("N", "Neuter"),
-    ("C", "Common"),
-    ("P", "Plural"),
-    ("S", "Singular");
+    ('F', 'Female'),
+    ('M', 'Male'),
+    ('N', 'Neuter'),
+    ('C', 'Common'),
+    ('P', 'Plural'),
+    ('S', 'Singular');
+
+-- ==============================================================================================================================
+-- EXTRA (FOR NAMES)
+-- ==============================================================================================================================
 
 CREATE TABLE IF NOT EXISTS lexicon.extra(
-    id          CHAR(1) PRIMARY KEY,
-    name        TEXT
+    id      TEXT PRIMARY KEY,
+    name    TEXT
 );
--- Extra for Names is L=Location, P=Person, LG/PG=Gentilic, T=Title (i.e. any other capitalised nouns such as titles, months, gods, planets etc)
-INSERT INTO lexicon.gender (id, name)
+
+-- Extra for Names: L=Location, P=Person, LG/PG=Gentilic, T=Title  (i.e. any other capitalised nouns such as titles, months, gods, planets etc)
+INSERT INTO lexicon.extra (id, name)
 VALUES
-    ("L"    , "Location"),
-    ("P"    , "Person"),
-    ("LG"   , "Gentilic"),
-    ("PG"   , "Gentilic"),
-    ("T"    , "Title");
+    ('L'    , 'Location'),
+    ('P'    , 'Person'),
+    ('LG'   , 'Gentilic'),
+    ('PG'   , 'Gentilic'),
+    ('T'    , 'Title');
+
+-- ==============================================================================================================================
+-- LEXICON SOURCES
+-- ==============================================================================================================================
 
 CREATE TABLE IF NOT EXISTS lexicon.entry(
-    id              CHAR(1) PRIMARY KEY,
-    name            TEXT,
-    description     TEXT
+    id          TEXT PRIMARY KEY,
+    name        TEXT,
+    description TEXT
 );
+
 INSERT INTO lexicon.entry (id, name, description)
 VALUES
-    ("AS"   , "Abbott Smith",
-        "from https://github.com/translatable-exegetical-tools/Abbott-Smith, with corrections and adapted by Tyndale Scholars. "),
+    ('AS', 'Abbott Smith',
+        'from https://github.com/translatable-exegetical-tools/Abbott-Smith, with corrections and adapted by Tyndale Scholars.'),
 
-    ("ML"   , "Middle Liddell",
-        "from Perseus - used for Meaning in the Brief lexicon when there is no entry by (AS)"),
+    ('ML', 'Middle Liddell',
+        'from Perseus - used for meaning in the Brief lexicon when there is no entry by Abbott-Smith (AS)'),
 
-    ("LSJ"  , "Liddell-Scott-Jones",
-        "from Perseus, with additional features and corrections by Tyndale House, Cambridge:
- - most abbreviations have been expanded, usually in accordance with the introductions to the lexicons but occasionally made clearer. 
- - dates have been added to authors by Tyndale House, using standard reference works. 
- - coding is added to hide bibliographic data under a link stating the earliest date included in that data. (Note, not all devices work well with tooltips, so consider implementing a clickable option such as http://jsfiddle.net/xaAN3/) ")
+    ('LSJ', 'Liddell-Scott-Jones',
+        'from Perseus, with additional features and corrections by Tyndale House, Cambridge.'),
 
-    ("MT"   , "Mounce's Teknia Greek dictionary",
-        "from www.billmounce.com/greek-dictionary (with permission) -  used for Meaning in the Brief lexicon when there is no entry by (AS) or (ML)"),
+    ('MT', 'Mounce''s Teknia Greek Dictionary',
+        'from www.billmounce.com/greek-dictionary (with permission). Used when there is no entry by AS or ML.'),
 
-    ("ABDB" , "Abridged BDB by Online Bible",
-        "© Larry Pierce of OnlineBible.net <olbsupport@onlinebible.net>. They are for guidance only. Permission should be gained from Online Bible before these are applied in any project. "),
+    ('ABDB', 'Abridged BDB by Online Bible',
+        '© Larry Pierce of OnlineBible.net. Permission should be obtained before applying in projects.'),
 
-    -- OR BADG??? 
-    ("BDB"  , "Brown–Driver–Briggs", 
-        None),
+    ('BDB', 'Brown–Driver–Briggs',
+        NULL),
 
-    ("BADG" , "Bauer–Danker–Arndt–Gingrich",
-        "")
+    ('BADG', 'Bauer–Danker–Arndt–Gingrich',
+        NULL),
 
-    ("CLBL" , "J. Green's A Concise Lexicon of the Biblical Languages",
-        "These definitions refer to J. Green's A Concise Lexicon of the Biblical Languages"),
+    ('CLBL', 'J. Green''s A Concise Lexicon of the Biblical Languages',
+        'Definitions refer to J. Green''s lexicon.'),
 
-    ("TWOT" , "Harris, Archer, & Waltke's Theological Wordbook of the Old Testament",
-        "These definitions refer to Harris, Archer, & Waltke's Theological Wordbook of the Old Testament"),
+    ('TWOT', 'Harris, Archer & Waltke''s Theological Wordbook of the Old Testament',
+        'Definitions refer to TWOT.'),
 
-    ("OPEN" , "OpenScripture's extended Strongs by Tyndale House Cambridge",
-        "These have been edited to conform with OpenScripture's extended Strongs by Tyndale House Cambridge. Alignment with dStrongs is not yet completed. ")
+    ('OPEN', 'OpenScripture''s Extended Strong''s',
+        'Edited to conform with OpenScripture''s extended Strong''s by Tyndale House Cambridge.');
 
+-- ==============================================================================================================================
+-- TRANSLATORS BRIEF LEXICON (HEBREW)
 -- ==============================================================================================================================
 
 CREATE TABLE IF NOT EXISTS lexicon.tbesh(
@@ -122,12 +142,16 @@ CREATE TABLE IF NOT EXISTS lexicon.tbesh(
     d_u_relationship    TEXT,
     u_strong            TEXT,
     iso                 TEXT,
-    text                TEXT, -- Original Language -> Hebrew
+    text                TEXT,   -- Original Language -> Hebrew
     transliteration     TEXT,
     morph               TEXT,
     gloss               TEXT,
     meaning             TEXT
 );
+
+-- ==============================================================================================================================
+-- TRANSLATORS BRIEF LEXICON (GREEK)
+-- ==============================================================================================================================
 
 CREATE TABLE IF NOT EXISTS lexicon.tbesg(
     id                  SERIAL PRIMARY KEY,
@@ -136,13 +160,17 @@ CREATE TABLE IF NOT EXISTS lexicon.tbesg(
     d_u_relationship    TEXT,
     u_strong            TEXT,
     iso                 TEXT,
-    text                TEXT, -- Original Language -> Greek
+    text                TEXT,   -- Original Language -> Greek
     transliteration     TEXT,
     morph               TEXT,
     gloss               TEXT,
     meaning             TEXT,
-    other_lexicon       TEXT, -- Abbot-Smith lexicon (AS), with gaps occassionally filled from edited versions of Middle LSJ
+    other_lexicon       TEXT    -- Abbot-Smith lexicon (AS), with gaps occassionally filled from edited versions of Middle LSJ
 );
+
+-- ==============================================================================================================================
+-- TRANSLATORS FORMATTED LSJ
+-- ==============================================================================================================================
 
 CREATE TABLE IF NOT EXISTS lexicon.tflsj(
     id                  SERIAL PRIMARY KEY,
@@ -151,12 +179,9 @@ CREATE TABLE IF NOT EXISTS lexicon.tflsj(
     d_u_relationship    TEXT,
     u_strong            TEXT,
     iso                 TEXT,
-    text                TEXT, -- Original Language -> Greek
+    text                TEXT,   -- Original Language -> Greek
     transliteration     TEXT,
     morph               TEXT,
     gloss               TEXT,
     meaning             TEXT    -- LSJ Meaning
 );
-
--- ==============================================================================================================================
-
