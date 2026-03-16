b916c86bdf454d9b41f60c7aa7768d3b583ad45b
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-03-16 15:25:08 +0000

    chore: added initialised codes for TEBSH for those missing from import with mapping to features and values

--- services/ingestor/stepbible/lexicon/TEBSH.py
+++ services/ingestor/stepbible/lexicon/TEBSH.py
@@ -8,7 +8,277 @@ from manager.managerhandler import ManagerHandler
 from ingestor.stepbible.lexicon.lexicon_base import LexiconBase
 
 CODES = {
-    
+    "Adv"       : {
+        "Function": "Adverb"
+    },
+    "Cond"      : {
+        "Function": "Conditional"
+    },
+    "Conj"      : {
+        "Function": "Conjunction"
+    },
+    "DemP"      : {
+        "Function": "Demonstrative Pronoun"
+    },
+    "IndP"      : {
+        "Function": "Indefinite Pronoun"
+    },
+    "Intg"      : {
+        "Function": "Interogative"
+    },
+    "Intj"      : {
+        "Function": "Interjection"
+    },
+    "N--TG"     : {
+        "Function": "Noun",
+        "Name type": "Title Gentilic"
+    },
+    "N-F-PG"    : {
+        "Function": "Noun",
+        "Gender": "Feminine",
+        "Name type": "Person Gentilic"
+    },
+    "N-F-T"     : {
+        "Function": "Noun",
+        "Gender": "Feminine",
+        "Name type": "Title"
+    },
+    "N-M-PG"    : {
+        "Function": "Noun",
+        "Gender": "Masculine",
+        "Name type": "Person Gentilic"
+    },
+    "Neg"       : {
+        "Function": "Negative"
+    },
+    "Op1c"      : {
+        "Function": "Suffix",
+        "Person": "1st",
+        "Form": "Common",
+        "Number": "Plural"
+    },
+    "Op2f"      : {
+        "Function": "Suffix",
+        "Person": "2nd",
+        "Gender": "Feminine",
+        "Number": "Plural"
+    },
+    "Op2m"      : {
+        "Function": "Suffix",
+        "Person": "2nd",
+        "Gender": "Masculine",
+        "Number": "Plural"
+    },
+    "Op3f"      : {
+        "Function": "Suffix",
+        "Person": "3rd",
+        "Gender": "Feminine",
+        "Number": "Plural"
+    },
+    "Op3m"      : {
+        "Function": "Suffix",
+        "Person": "3rd",
+        "Gender": "Masculine",
+        "Number": "Plural"
+    },
+    "Os1c"      : {
+        "Function": "Suffix",
+        "Person": "1st",
+        "Form": "Common",
+        "Number": "Singular"
+    },
+    "Os2f"      : {
+        "Function": "Suffix",
+        "Person": "2nd",
+        "Gender": "Feminine",
+        "Number": "Singular"
+    },
+    "Os2m"      : {
+        "Function": "Suffix",
+        "Person": "2nd",
+        "Gender": "Masculine",
+        "Number": "Singular"
+    },
+    "Os3f"      : {
+        "Function": "Suffix",
+        "Person": "3rd",
+        "Gender": "Feminine",
+        "Number": "Singular"
+    },
+    "Os3m"      : {
+        "Function": "Suffix",
+        "Person": "3rd",
+        "Gender": "Masculine",
+        "Number": "Singular"
+    },
+    "Part"      : {
+        "Function": "Particle"
+    },
+    "PerP-CP"   : {
+        "Function": "Personal Pronoun",
+        "Form": "Common",
+        "Number": "Plural"
+    },
+    "PerP-CS"   : {
+        "Function": "Personal Pronoun",
+        "Form": "Common",
+        "Number": "Singular"
+    },
+    "PerP-FP"   : {
+        "Function": "Personal Pronoun",
+        "Gender": "Feminine",
+        "Number": "Plural"
+    },
+    "PerP-FS"   : {
+        "Function": "Personal Pronoun",
+        "Gender": "Feminine",
+        "Number": "Singular"
+    },
+    "PerP-MP"   : {
+        "Function": "Personal Pronoun",
+        "Gender": "Masculine",
+        "Number": "Plural"
+    },
+    "PerP-MS"   : {
+        "Function": "Personal Pronoun",
+        "Gender": "Masculine",
+        "Number": "Singular"
+    },
+    "Pp1c"      : {
+        "Function": "Suffix",
+        "Form": "Common",
+        "Number": "Plural",
+        "Person": "1st"
+    },
+    "Pp2f"      : {
+        "Function": "Suffix",
+        "Gender": "Feminine",
+        "Person": "2nd",
+        "Number": "Plural",
+    },
+    "Pp2m"      : {
+        "Function": "Suffix",
+        "Gender": "Masculine",
+        "Person": "2nd",
+        "Number": "Plural",
+    },
+    "Pp3f"      : {
+        "Function": "Suffix",
+        "Gender": "Feminine",
+        "Person": "3rd",
+        "Number": "Plural",
+    },
+    "Pp3m"      : {
+        "Function": "Suffix",
+        "Gender": "Masculine",
+        "Person": "3rd",
+        "Number": "Plural",
+    },
+    "Prefix"    : {
+        "Function": ""
+    },
+    "Prep"      : {
+        "Function": "Preposition"
+    },
+    "Punct."    : {
+        "Function": "Punctuation"
+    },
+    "Ps1c"      : {
+        "Function": "Suffix",
+        "Form": "Common",
+        "Person": "1st",
+        "Number": "Singular",
+    },
+    "Ps2f"      : {
+        "Function": "Suffix",
+        "Gender": "Feminine",
+        "Person": "2nd",
+        "Number": "Singular",
+    },
+    "Ps2m"      : {
+        "Function": "Suffix",
+        "Gender": "Masculine",
+        "Person": "2nd",
+        "Number": "Singular",
+    },
+    "Ps3f"      : {
+        "Function": "Suffix",
+        "Gender": "Feminine",
+        "Person": "3rd",
+        "Number": "Singular",
+    },
+    "Ps3m"      : {
+        "Function": "Suffix",
+        "Gender": "Masculine",
+        "Person": "3rd",
+        "Number": "Singular",
+    },
+    "RelP"      : {
+        "Function": "Relative Pronoun"
+    },
+    "Sp1c"      : {
+        "Function": "Suffix",
+        "Form": "Common",
+        "Person": "1st",
+        "Number": "Plural",
+    },
+    "Sp2f"      : {
+        "Function": "Suffix",
+        "Gender": "Feminine",
+        "Person": "2nd",
+        "Number": "Plural",
+    },
+    "Sp2m"      : {
+        "Function": "Suffix",
+        "Gender": "Masculine",
+        "Person": "2nd",
+        "Number": "Plural",
+    },
+    "Sp3f"      : {
+        "Function": "Suffix",
+        "Gender": "Feminine",
+        "Person": "3rd",
+        "Number": "Plural",
+    },
+    "Sp3m"      : {
+        "Function": "Suffix",
+        "Gender": "Masculine",
+        "Person": "3rd",
+        "Number": "Plural",
+    },
+    "Ss1c"      : {
+        "Function": "Suffix",
+        "Form": "Common",
+        "Person": "1st",
+        "Number": "Singular",
+    },
+    "Ss2f"      : {
+        "Function": "Suffix",
+        "Gender": "Feminine",
+        "Person": "2nd",
+        "Number": "Singular",
+    },
+    "Ss2m"      : {
+        "Function": "Suffix",
+        "Gender": "Masculine",
+        "Person": "2nd",
+        "Number": "Singular",
+    },
+    "Ss3f"      : {
+        "Function": "Suffix",
+        "Gender": "Feminine",
+        "Person": "3rd",
+        "Number": "Singular",
+    },
+    "Ss3m"      : {
+        "Function": "Suffix",
+        "Gender": "Masculine",
+        "Person": "3rd",
+        "Number": "Singular",
+    },
+    "Suffix"    : {
+        "Function": ""
+    },
 }
 
 class TEBSH(LexiconBase):
