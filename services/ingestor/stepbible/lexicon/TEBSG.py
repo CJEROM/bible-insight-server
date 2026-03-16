2c6444ab12fd9c4f00a253c8af7d40f501e56a1f
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-03-16 14:38:23 +0000

    chore: modified TEBSG to now inherit correctly from LexiconBase

--- services/ingestor/stepbible/lexicon/TEBSG.py
+++ services/ingestor/stepbible/lexicon/TEBSG.py
@@ -4,27 +4,173 @@
 from ingestor.stepbible.download_file import DownloadFile
 
 from manager.managerhandler import ManagerHandler
-from manager.logmanager import LogManager
 
-from database.boundary.step_bible_boundary import StepBibleReadBoundary, StepBibleWriteBoundary, StepBibleDeleteBoundary
+from ingestor.stepbible.lexicon.lexicon_base import LexiconBase
 
-import re
+CODES = {
+    "A"      : {
+        "Function": "Adjective",
+    },
+    "A--C"   : {
+        "Function": "Adjective",
+        "Extra": "Comparative"
+    },
+    "A--L"   : {
+        "Function": "Adjective",
+        "Name type": "Location"
+    },
+    "A--LG"  : {
+        "Function": "Adjective",
+        "Name type": "Location Gentilic"
+    },
+    "A--PG"  : {
+        "Function": "Adjective",
+        "Name type": "Person Gentilic"
+    },
+    "A--S"   : {
+        "Function": "Adjective",
+        "Extra": "Superlative"
+    },
+    "A-F"    : {
+        "Function": "Adjective",
+        "Gender": "Feminine"
+    },
+    "A-M"    : {
+        "Function": "Adjective",
+        "Gender": "Masculine"
+    },
+    "C"      : {
+        "Function": "Reciprocal Pronoun"
+    },
+    "D"      : {
+        "Function": "Demonstrative pronoun"
+    },
+    "F-1"    : {
+        "Function": "Reflexive Pronoun",
+        "Person": "1st"
+    },
+    "F-2"    : {
+        "Function": "Reflexive Pronoun",
+        "Person": "2nd"
+    },
+    "F-3"    : {
+        "Function": "Reflexive Pronoun",
+        "Person": "3rd"
+    },
+    "I"      : {
+        "Form": "Interrogative"
+    },
+    "K"      : {
+        "Function": "Correlative"
+    },
+    "N"      : {
+        "Function": "Noun"
+    },
+    "N--L"   : {
+        "Function": "Noun",
+        "Name type" : "Location"
+    },
+    "N--LG"  : {
+        "Function": "Noun",
+        "Name type": "Location Gentilic"
+    },
+    "N--PG"  : {
+        "Function": "Noun",
+        "Name type": "Person Gentilic"
+    },
+    "N--T"   : {
+        "Function": "Noun",
+        "Name type": "Title"
+    },
+    "N-F"    : {
+        "Function": "Noun",
+        "Gender": "Feminine"
+    },
+    "N-F-L"  : {
+        "Function": "Noun",
+        "Gender": "Feminine",
+        "Name type" : "Location"
+    },
+    "N-F-LG" : {
+        "Function": "Noun",
+        "Gender": "Feminine",
+        "Name type" : "Location Gentilic"
+    },
+    "N-F-P"  : {
+        "Function": "Noun",
+        "Gender": "Feminine",
+        "Name type" : "Person"
+    },
+    "N-M"    : {
+        "Function": "Noun",
+        "Gender": "Masculine",
+    },
+    "N-M-L"  : {
+        "Function": "Noun",
+        "Gender": "Masculine",
+    },
+    "N-M-LG" : {
+        "Function": "Noun",
+        "Gender": "Masculine",
+    },
+    "N-M-P"  : {
+        "Function": "Noun",
+        "Gender": "Masculine",
+    },
+    "N-M-T"  : {
+        "Function": "Noun",
+        "Gender": "Masculine",
+        "Name type": "Title"
+    },
+    "N-N"    : {
+        "Function": "Noun",
+        "Gender": "Neuter"
+    },
+    "P"      : {
+        "Function": "Personal Pronoun"
+    },
+    "P-1"    : {
+        "Function": "Personal Pronoun",
+        "Person": "1st"
+    },
+    "P-2"    : {
+        "Function": "Personal Pronoun",
+        "Person": "2nd"
+    },
+    "Q"      : {
+        "Function": "Correlative or Interrogative"
+    },
+    "R"      : {
+        "Function": "Relative Pronoun"
+    },
+    "S-1"    : {
+        "Function": "Possessive Pronoun",
+        "Person": "1st"
+    },
+    "S-2"    : {
+        "Function": "Possessive Pronoun",
+        "Person": "2nd"
+    },
+    "T"      : {
+        "Function": "Article"
+    },
+    "V"      : {
+        "Function": "Verb"
+    },
+    "X"      : {
+        "Function": "Indefinite Pronoun"
+    },
+}
 
-class TEBSG():
-    def __init__(self, 
-            manager: ManagerHandler, 
-            log: LogManager
-        ):
-
-        self.manager        = manager
-        self.log            = log
-        self.db             = manager.get_db()
-
-        self.read           = StepBibleReadBoundary(self.db)
-        self.write          = StepBibleWriteBoundary(self.db)
-
-        self.process_file()
-
+class TEBSG(LexiconBase):
+    def __init__(self, manager, log):
+        super().__init__(
+            manager         = manager, 
+            log             = log, 
+            start_marker    = "eStrong	dStrong	uStrong	Greek	Transliteration	Morph	Gloss	Abbott-Smith lexicon (AS), with gaps occationally filled from edited versions of  Middle LSJ ",
+            codes           = CODES
+        )
+        
     def download_files(self):
 
         FILE = DownloadFile(
@@ -46,157 +192,7 @@ class TEBSG():
             license_code    = "CC BY 4.0",
             is_new_license  = False
         )
-
-        return FILE
-
-    def process_file(self):
-        downloaded_file = self.download_files()
-
-        # Read the downloaded file
-        with open(downloaded_file.this_file_path, "r", encoding="utf-8") as f:
-            # Loop through file line by line
-            valid = False
-
-            for line in f:
-                if line.startswith("eStrong	dStrong	uStrong	Greek	Transliteration	Morph	Gloss	Abbott-Smith lexicon (AS), with gaps occationally filled from edited versions of  Middle LSJ "):
-                    valid = True
-                    continue
-                
-                # Skip the empty lines so we can start ingesting the data
-                if line.strip() == "" or line.startswith("==="):
-                    continue
-
-                if valid:
-                    columns = line.split("\t")
-
-                    morph_code = columns[5] if columns[5].strip() != "" else None
-
-                    data_id = self.write.write_lexicon_data(
-                        e_strong            = columns[0],
-                        d_strong            = self. get_strong_relationship(
-                                                raw_d_strong    = columns[1],
-                                                result          = 0
-                                            ),
-                        d_u_relationship    = self. get_strong_relationship(
-                                                raw_d_strong    = columns[1],
-                                                result          = 1
-                                            ),
-                        u_strong            = columns[2],
-                        text                = columns[3],
-                        transliteration     = columns[4],
-                        morph               = morph_code,
-                        gloss               = columns[6],
-                        meaning             = columns[7].strip(),
-                        source_id           = downloaded_file.source_id
-                    )
-
-                    if morph_code:
-                        self.process_morph_code(
-                            data_id = data_id,
-                            code    = morph_code
-                        )
-        
-        self.db.commit()
-
-    import re
-
-    def process_morph_code(self, data_id: int, code: str):
-
-        # -------------------------------------------------------
-        # 1. Expand feature alternates (case 3 -> case 4)
-        # Example: G:N-M/F -> G:N-M / G:N-F
-        # -------------------------------------------------------
-
-        match = re.search(r"([A-Z]:[A-Z]+-[^/\s]+)/([^\s+]+)", code)
-
-        if match:
-            base_left = match.group(1)      # e.g. G:N-M
-            alt = match.group(2)            # e.g. F
-
-            prefix = base_left.rsplit("-", 1)[0]  # G:N
-
-            left = base_left
-            right = f"{prefix}-{alt}"
-
-            expanded = code.replace(match.group(0), f"{left} / {right}")
-
-            # Re-run parser with expanded string
-            return self.process_morph_code(data_id, expanded)
-
-        # -------------------------------------------------------
-        # 2. Split components normally
-        # -------------------------------------------------------
-
-        components = re.split(r"(\s*[+/]\s*)", code)
-
-        relation_type = "single"
-
-        position = 1
-
-        for part in components:
-
-            # ---------------------------------------------------
-            # Detect relationship separators
-            # ---------------------------------------------------
-
-            if part == '+':
-                relation_type = "compound"
-                continue
-
-            elif part == ' + ':
-                relation_type = "multi_word"
-                continue
-
-            elif part.strip() == '/':
-                relation_type = "alternate"
-                continue
-
-            # ---------------------------------------------------
-            # Otherwise it's a morphology code
-            # ---------------------------------------------------
-
-            language = None
-            sub_code = None
-
-            if ":" in part:
-                language, sub_code = part.split(":", 1)
-
-            else:
-                # Observed only in Hebrew entries
-                language = "H"
-                sub_code = part
-
-            if self.read.find_morph_code(sub_code) == None:
-                print(f"\t{sub_code}")
-            else:
-                self.write.map_lexicon_morph_codes(
-                    data_id         = data_id,
-                    position        = position,
-                    relation_type   = relation_type,
-                    language        = language,
-                    sub_code        = sub_code
-                )
-
-            position += 1
-
-    def get_strong_relationship(self,
-            raw_d_strong    : str,
-            result          : int # 0 (d_strong) | 1 (relationship)
-        ):
-        d_strong = raw_d_strong.split("=")[0].strip()
-
-        relationship = None
-        if len(raw_d_strong.split("=")) < 2:
-            relationship = "="
-        else:
-            relationship = raw_d_strong.split("=")[1].strip()
-
-        if result == 0:
-            return d_strong
-        elif result == 1:
-            return relationship
-        else: 
-            return None
+        self.process_file(FILE)
 
 if __name__ == "__main__":
     manager = ManagerHandler()
