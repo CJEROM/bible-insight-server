4c025d0fd10a98e8e09e51d4b5590c121a796fbb
CJEROM <118741301+CJEROM@users.noreply.github.com>
2026-03-17 19:34:46 +0000

    feature: set up Case Feature Code class with inheritance from new FeatureBase

--- services/ingestor/stepbible/morphology/codes/base/case_code.py
+++ services/ingestor/stepbible/morphology/codes/base/case_code.py
@@ -1,15 +1,37 @@
+from ingestor.stepbible.morphology.codes.feature_base import FeatureBase
 
-from ingestor.stepbible.morphology.codes.base_parser import BaseParser
+class CaseCode(FeatureBase):
+    # Format -> "Name": {"Codes": [""], "Description": ""}
+    CODES       = "Codes"
+    DESCRIPTION = "Description"
 
-class CaseCode(BaseParser):
-    # Vocative
-    # Nominative
-    # Dative
-    # Accusative
-    # Genitive
+    values = {
+        "Accusative": {
+            CODES           : ["A"],
+            DESCRIPTION     : ""
+        },
+        "Dative": {
+            CODES           : ["D"],
+            DESCRIPTION     : ""
+        },
+        "Genitive": {
+            CODES           : ["G"],
+            DESCRIPTION     : ""
+        },
+        "Nominative": {
+            CODES           : ["N"],
+            DESCRIPTION     : ""
+        },
+        "Vocative": {
+            CODES           : ["V"],
+            DESCRIPTION     : ""
+        }
+    }
+
     def __init__(self, manager, log, code):
-        super().__init__(
-            manager = manager, 
-            log     = log, 
-            code    = code
-        )
+        name        = "Case"
+        description = ""
+        limit       = 1
+        union       = None
+
+        super().__init__(manager, log, code, name, description, limit, union)
